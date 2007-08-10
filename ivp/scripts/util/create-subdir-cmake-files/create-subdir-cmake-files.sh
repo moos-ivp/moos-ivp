2214aacc371de57712df080898049f587b74b8a1
Christian Convey <christian.convey@navy.mil>
2007-08-10 18:38:01 +0000

    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@48 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/scripts/util/create-subdir-cmake-files/create-subdir-cmake-files.sh
+++ ivp/scripts/util/create-subdir-cmake-files/create-subdir-cmake-files.sh
@@ -1,6 +1,42 @@
 #!/bin/sh
 
 set -e 
+
+#===============================================================================
+
+print_usage() {
+   echo `basename ${0}` "<src-dir>"
+   echo
+   echo "Writes the CMakeLists.txt file for each subdirectory of <src-dir>."
+   echo "This script uses built-in information about what subdirectories should "
+   echo "exist, and how each subdirectory's CMakeLists.txt file should look."
+}
+
+#===============================================================================
+
+parse_and_validate_cmd_line() {
+   if [ "$#" -ne "1" ]; then
+      print_usage
+      exit 1
+   fi 
+   
+   if [ -h $0 ]; then
+      echo "You're running this script as a symbolic link.  Please don't: doing so "
+      echo "makes it hard for this script to find some helper files that are supposed "
+      echo "to reside in the same directory as the script itself."
+      exit 1
+   fi
+   
+   SCRIPT_DIR=`dirname ${0}`
+   
+   if [ ! -d "${1:?}" ]; then
+      echo "<src-dir> doesn't seem to exist, or isn't a directory."
+      exit 1
+   fi
+   
+   SRC_DIR=${1}
+}
+
 #===============================================================================
 
 simple_lib_cmake_file() {
@@ -17,37 +53,23 @@ simple_lib_cmake_file() {
       exit 1
    fi
 
+   OUTPUT_FILE=${SRC_DIR}/${SUBDIR:?}/CMakeLists.txt
+
+   echo "About to create file:" ${OUTPUT_FILE}
+
    cmake \
       -DINPUT_FILE=${SCRIPT_DIR}/simple-lib-template.txt     \
-      -DOUTPUT_FILE=${SUBDIR:?}/CMakeLists.txt \
+      -DOUTPUT_FILE=${OUTPUT_FILE} \
       -DLIBNAME=${LIBNAME} \
       -DCMAKE_BACKWARDS_COMPATIBILITY:STRING=2.4            \
       -P ${SCRIPT_DIR}/customize-template-file.cmake 
 }
 
 #===============================================================================
-
-# p   rint_usage_and_exit() {
-# }
-
+# Main code...
 #===============================================================================
 
-if [ -h $0 ]; then
-   echo "You're running this script as a symbolic link.  Please don't: doing so "
-   echo "makes it hard for this script to find some helper files that are supposed "
-   echo "to reside in the same directory as the script itself."
-   exit 1
-fi
-
-SCRIPT_DIR=`dirname ${0}`
-
-if [ ! -d "${1:?}" ]; then
-   echo "<src-dir> doesn't seem to exist, or isn't a directory."
-   exit 1
-fi
-
-SRC_DIR=${1}
-
+parse_and_validate_cmd_line $*
 
 SUBDIR=lib_anrp_util         LIBNAME=anrp_util         simple_lib_cmake_file
 SUBDIR=lib_artifacts         LIBNAME=artifacts         simple_lib_cmake_file
