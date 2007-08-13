602309e3e55535b171fb753bef1270fcff48e238
Christian Convey <christian.convey@navy.mil>
2007-08-13 17:47:58 +0000

    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@49 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/scripts/util/create-subdir-cmake-files/create-subdir-cmake-files.sh
+++ ivp/scripts/util/create-subdir-cmake-files/create-subdir-cmake-files.sh
@@ -66,31 +66,65 @@ simple_lib_cmake_file() {
 }
 
 #===============================================================================
+
+lib_with_compile_flags_cmake_file() {
+   if [ ! -d ${SRC_DIR}/${SUBDIR:?} ]; then
+      echo "Something is wrong.  Directory ${SRC_DIR}/${SUBDIR:?} doesn't exist."
+      exit 1
+   fi
+
+   if [ ! -f ${SCRIPT_DIR}/lib-with-compile-flags-template.txt ]; then
+      echo "I should be able to find the file ./lib-with-compile-flags-template.txt, "
+      echo "but I can't."
+      echo ""
+      echo "You're supposed to run this script from it's own directory."
+      exit 1
+   fi
+
+   OUTPUT_FILE=${SRC_DIR}/${SUBDIR:?}/CMakeLists.txt
+
+   echo "About to create file:" ${OUTPUT_FILE}
+
+   cmake \
+      -DINPUT_FILE=${SCRIPT_DIR}/lib-with-compile-flags-template.txt     \
+      -DOUTPUT_FILE=${OUTPUT_FILE} \
+      -DLIBNAME=${LIBNAME} \
+      -DIVP_DATA_DIR='${IVP_DATA_DIR}' \
+      -DCMAKE_BACKWARDS_COMPATIBILITY:STRING=2.4            \
+      -P ${SCRIPT_DIR}/customize-template-file.cmake 
+}
+
+#===============================================================================
 # Main code...
 #===============================================================================
 
 parse_and_validate_cmd_line $*
 
-SUBDIR=lib_anrp_util         LIBNAME=anrp_util         simple_lib_cmake_file
-SUBDIR=lib_artifacts         LIBNAME=artifacts         simple_lib_cmake_file
-SUBDIR=lib_behaviors         LIBNAME=behaviors         simple_lib_cmake_file
-SUBDIR=lib_behaviors-attic   LIBNAME=behaviors-attic   simple_lib_cmake_file
-SUBDIR=lib_behaviors-colregs LIBNAME=behaviors-colregs simple_lib_cmake_file
-SUBDIR=lib_behaviors-don     LIBNAME=behaviors-don     simple_lib_cmake_file
-SUBDIR=lib_behaviors-marine  LIBNAME=behaviors-marine  simple_lib_cmake_file
-SUBDIR=lib_behaviors-oxford  LIBNAME=behaviors-oxford  simple_lib_cmake_file
-SUBDIR=lib_behaviors-sandbox LIBNAME=behaviors-sandbox simple_lib_cmake_file
-SUBDIR=lib_bhvutil           LIBNAME=bhvutil           simple_lib_cmake_file
-SUBDIR=lib_genutil           LIBNAME=genutil           simple_lib_cmake_file
-SUBDIR=lib_geometry          LIBNAME=geometry          simple_lib_cmake_file
-SUBDIR=lib_helmivp           LIBNAME=helmivp           simple_lib_cmake_file
-SUBDIR=lib_ipfview           LIBNAME=ipfview           simple_lib_cmake_file
-SUBDIR=lib_ivpbuild          LIBNAME=ivpbuild          simple_lib_cmake_file
-SUBDIR=lib_ivpbuild-extra    LIBNAME=ivpbuild-extra    simple_lib_cmake_file
-SUBDIR=lib_ivpcore           LIBNAME=ivpcore           simple_lib_cmake_file
-SUBDIR=lib_logic             LIBNAME=logic             simple_lib_cmake_file
-SUBDIR=lib_marineview        LIBNAME=marineview        simple_lib_cmake_file
-SUBDIR=lib_mbutil            LIBNAME=mbutil            simple_lib_cmake_file
-SUBDIR=lib_mbutiltest        LIBNAME=mbutiltest        simple_lib_cmake_file
-SUBDIR=lib_navplot           LIBNAME=navplot           simple_lib_cmake_file
-SUBDIR=lib_track             LIBNAME=opt               simple_lib_cmake_file
+# SUBDIR=lib_anrp_util         LIBNAME=anrp_util         simple_lib_cmake_file
+# SUBDIR=lib_artifacts         LIBNAME=artifacts         simple_lib_cmake_file
+# SUBDIR=lib_behaviors         LIBNAME=behaviors         simple_lib_cmake_file
+# SUBDIR=lib_behaviors-attic   LIBNAME=behaviors-attic   simple_lib_cmake_file
+# SUBDIR=lib_behaviors-colregs LIBNAME=behaviors-colregs simple_lib_cmake_file
+# SUBDIR=lib_behaviors-don     LIBNAME=behaviors-don     simple_lib_cmake_file
+# SUBDIR=lib_behaviors-marine  LIBNAME=behaviors-marine  simple_lib_cmake_file
+# SUBDIR=lib_behaviors-oxford  LIBNAME=behaviors-oxford  simple_lib_cmake_file
+# SUBDIR=lib_behaviors-sandbox LIBNAME=behaviors-sandbox simple_lib_cmake_file
+# SUBDIR=lib_bhvutil           LIBNAME=bhvutil           simple_lib_cmake_file
+# SUBDIR=lib_genutil           LIBNAME=genutil           simple_lib_cmake_file
+# SUBDIR=lib_geometry          LIBNAME=geometry          simple_lib_cmake_file
+# SUBDIR=lib_helmivp           LIBNAME=helmivp           simple_lib_cmake_file
+# SUBDIR=lib_ipfview           LIBNAME=ipfview           simple_lib_cmake_file
+# SUBDIR=lib_ivpbuild          LIBNAME=ivpbuild          simple_lib_cmake_file
+# SUBDIR=lib_ivpbuild-extra    LIBNAME=ivpbuild-extra    simple_lib_cmake_file
+# SUBDIR=lib_ivpcore           LIBNAME=ivpcore           simple_lib_cmake_file
+# SUBDIR=lib_logic             LIBNAME=logic             simple_lib_cmake_file
+# SUBDIR=lib_mbutil            LIBNAME=mbutil            simple_lib_cmake_file
+# SUBDIR=lib_mbutiltest        LIBNAME=mbutiltest        simple_lib_cmake_file
+# SUBDIR=lib_navplot           LIBNAME=navplot           simple_lib_cmake_file
+# SUBDIR=lib_track             LIBNAME=opt               simple_lib_cmake_file
+
+
+IVP_DATA_DIR="${SRC_DIR}/../data/" \
+SUBDIR=lib_marineview \
+LIBNAME=marineview \
+lib_with_compile_flags_cmake_file
