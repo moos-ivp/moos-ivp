2b0a500cb4a9f5349d963a40a466437885534a3f
Christian Convey <christian.convey@navy.mil>
2007-09-07 03:09:10 +0000

    Lots of enhancements to slog-filt.py
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@158 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/src/bin/slog-filt.py
+++ ivp/src/bin/slog-filt.py
@@ -55,53 +55,160 @@ def parse_slog_header(infile):
 
 #===============================================================================
 
+def get_matching_col_name(c, col_names):
+   """
+   If 'c' matches any of the elements of 'col_names' using a case-insensitive
+   string comparison, this returns that element from 'col_names'.
+   (This function assumes that at most one match will occur.)
+   
+   Otherwise this returns None.
+   """
+   for name in col_names:
+      if c.capitalize() == name.capitalize():
+         return name
+      
+   return None
+
+#===============================================================================
+
+def invert_dict(d):
+   """
+   Returns a dictionary whose keys and values have swapped places.
+   
+   Should crash if the values aren't all unique.
+   """
+   inverted_d = {}
+   for (key, val) in d.iteritems():
+      inverted_d[val] = key
+      
+   return inverted_d
+
+#===============================================================================
+
 def print_help_and_exit():
-   s = "Usage: slog-filt.py <in-file> --[hide]vars var1 [var2, ...] [--output <out-file>]"
+   s = \
+      "Usage: \n" + \
+      "To specify the output column using the command line:\n" + \
+      "   slog-filt.py <in-file> --[hide]vars var1 [var2, ...] [--select <query>] [--output <out-file>]\n" + \
+      "or\n" + \
+      "To specify the output column interactively:\n" + \
+      "   slog-filt.py <in-file> [--select <query>] [--output <out-file>]\n"
+   
    sys.exit(s)
 
 #===============================================================================
 
-def parse_cmdline_args(argv):
-   """ Tries to create a dictionary with the following elements:
-   'input-filename': Path/filename of the slog file to read in.
-   
-   Exactly one of the following:
-   'vars': A list of the variable names to appear in this program's output.
-   'hidevars': A list of variable names that are to *not* appear in this 
-      program's output.
-      
-   'outfile': A reference to the file object to which this program's output is
-      to be written.
-         
-   If something goes wrong, this function calls print_help_and_exit.
+def get_output_vars_by_prompting(var_name_to_col_number):
    """
-      
-   if len(argv) < 4:
-      print_help_and_exit()
-      
-   d = {
-      'input-filename':argv[1]
-      }
+   Prompts the user for a list of column names he wants to appear in the 
+   filtered output.  Returns that list.   
+   """
+   cols_to_output = []
+   col_number_to_var_name = invert_dict(var_name_to_col_number)
    
-   # User was supposed specify exactly one of '--vars' or '--hidevars'...
+   for col_idx in range(1, len(col_number_to_var_name)+1):
+      col_name = col_number_to_var_name[col_idx]
+         
+      satisfied = False
+      while not satisfied:
+         choice = raw_input("Output " + col_name + " [Y]: ").capitalize()
+         if choice in ['N', 'NO']:
+            satisfied = True
+         elif choice in ['', 'Y' 'YES']:
+            cols_to_output.append(col_name)
+            satisfied = True
+         else:
+            print "Please enter Y or N."        
+
+   return cols_to_output
+
+#===============================================================================
+
+def get_output_vars_list(argv, var_name_to_col_number):
+   
+   # How does the user want to specify the output columns? ...
    if '--vars' in argv:
       if '--hidevars' in argv:
          print_help_and_exit()
       else:
-         var_spec_flag='vars'
+         first_var_idx = argv.index('--vars') + 1
+         spec_type='vars'
    elif '--hidevars' in argv:
-         var_spec_flag='hidevars'
+         first_var_idx = argv.index('--hidevars') + 1
+         spec_type='hidevars'
    else:
-      print_help_and_exit()
-   vars_idx = argv.index('--' + var_spec_flag)
-
-
-   if vars_idx != 2:
-      print_help_and_exit()
-
-   if argv[-2] == '--output':
-      d[var_spec_flag] = argv[(vars_idx + 1):-3]
+      spec_type='interactive'
+   
+   
+   if spec_type == 'interactive':
+      output_vars_list = get_output_vars_by_prompting(var_name_to_col_number)
+   else:
+      # Figure out what columns the user specified.  Stop when we get to the
+      # next command-line arg that begins with "--", or when we run out of
+      # args...
+      user_var_list = []
+      for arg in argv[first_var_idx :] :
+         if arg.startswith("--"):
+            break
+         user_var_list.append(arg)
       
+      # Courtesy hack: If a column specified by the use doesn't appear in the
+      # slog file, but it *does* match if we ignore case, then adjust the user-
+      # provided column name's case accordingly...       
+      unmatchable_names = []
+      
+      for i in range(0, len(user_var_list)):
+         user_var_name = user_var_list[i]
+         matching_col_name = get_matching_col_name(user_var_name, 
+               var_name_to_col_number.keys())
+               
+         if user_var_name != matching_col_name:
+            if matching_col_name == None:
+               unmatchable_names.append(user_var_name)
+            else:
+               user_var_list[i] = matching_col_name
+      
+      
+      if len(unmatchable_names) > 0:
+         print >> sys.stderr, \
+            "You listed some variables that aren't in the supplied slog file:\n" + \
+            ", ".join(unmatchable_names)
+         print_help_and_exit()
+
+
+      if spec_type == 'vars':
+         # Include the variables in the sequence specified by the user.
+         output_vars_list = user_var_list
+      else:
+         # Include the variables in the sequence in which they appear in the 
+         # input slog file...
+         output_vars_list = []
+         col_number_to_var_name = invert_dict(var_name_to_col_number)
+   
+         for col_idx in range(1, len(col_number_to_var_name)+1):
+            col_name = col_number_to_var_name[col_idx]
+            if not col_name in user_var_set:
+               output_vars_list.append(var_name)
+         
+   
+   return output_vars_list
+   
+#===============================================================================
+
+def get_input_file(argv):
+   filename=argv[1]
+   
+   if not os.path.exists(filename):
+      print >> sys.stderr, "The file " + filename + \
+         " doesn't seem to exist."
+      print_help_and_exit()
+
+   return open(filename, "r")
+
+#===============================================================================
+
+def get_output_file(argv):
+   if argv[-2] == '--output':
       filename = argv[-1]
       
       if os.path.exists(filename):
@@ -109,28 +216,96 @@ def parse_cmdline_args(argv):
             " already exists (I don't overwrite files.)"
          print_help_and_exit()
       
-      d['outfile'] = file(filename, 'w')
+      return file(filename, 'w')
    else:      
-      d[var_spec_flag] = argv[(vars_idx + 1):]
-      d['outfile'] = sys.stdout
+      return sys.stdout
+
+#===============================================================================
+
+def split_query_string(query_string):
+   # If the first or last symbol in query_string is a '@', then the first or
+   # last element (respectively) in query_split will be the empty string.  
+   # Eliminate those - they make subsequent processing harder if left in...
+   query_split = query_string.split('@')
+   
+   if query_string[0] == '@':
+      assert(query_split[0] == "")
+      query_split = query_split[1:]
+   
+   if query_string[-1] == '@':
+      assert(query_split[-1] == "")
+      query_split = query_split[:-1]
+
+   return query_split
+
+#===============================================================================
+
+def find_col_names_in_query(query_string, query_split, valid_col_names):
+   """
+   'query_split' must be the result of split_query_string().
+   
+   Returns a dictionary.  Keys are indices into the list produced by splitting
+   the query string.
+   
+   Dependent values are either None (if a match couldn't be found) or
+   the name of the valid column name that matches.
+   """
+   return_dict = {}
+   
+   # Do some basic validation: Are all symbols valid?
+   # The basic structure should be [...][@var@[...]]*
+   # So we ought to see an even number of '@' symbols.  That means that if we
+   # split the string around the '@' symbols, we ought to have an odd number of
+   # elements.
+   #
+   # Also, if we split('@') the query string, either all even numbered, or all 
+   # odd numbered,elements in the resulting list are variable names, and we can 
+   # know which are which by just looking at the first character in the query 
+   # string.
+
+   if (query_string.count('@') % 2) == 1:
+      print >> sys.stderr, "The query string looks wrong: it has an odd number of '@' symbols"
+      print_help_and_exit()
       
-   # Make sure there are no duplicate entries in 'vars' or 'hidevars'.  That
-   # makes the rest of our code somewhat more complicated (or else the output
-   # is messed up), and I can't think of a case where the user is likely to 
-   # need it. -CJC
-   var_list = d[var_spec_flag]
-   for i in range(len(var_list)):
-      var_name = var_list[i]      
+   if query_string[0] == '@':
+      query_col_name_indices = range(0, len(query_split), 2)
+   else:
+      query_col_name_indices = range(1, len(query_split), 2)
+   
+   for i in query_col_name_indices:
+      col_name = query_split[i]
+      return_dict[i] = get_matching_col_name(col_name, valid_col_names)
+
+   return return_dict
+   
+#===============================================================================
+
+def get_query_expression(argv, var_name_to_col_number):
+   if not "--select" in argv:
+      return None
+   
+   select_idx = argv.index("--select")
+   if select_idx == (len(argv) - 1):
+      print >> sys.stderr, "You need a query string to follow '--select'"
+      print_help_and_exit()
       
-      # This finds the *first* occurrence of 'var_name'.  If 'i' is greater than
-      # the index of the first occurrence, we know we have two occurrences, 
-      # which is what we're testing for.
-      idx1 = var_list.index(var_name)
-      if idx1 != i:
-         print >> sys.stderr, "Each var name can appear at most once."
-         print_help_and_exit()
-      
-   return d
+   return argv[select_idx + 1]
+
+#===============================================================================
+            
+def validate_query_string(query_string, valid_col_names):
+   query_split = split_query_string(query_string)
+   query_col_names = find_col_names_in_query(query_string, query_split, valid_col_names)
+   
+   unmatched_names = []
+   for (idx, name) in query_col_names.iteritems():
+      if name == None:
+         unmatched_names.append(query_split[idx])
+         
+   if len(unmatched_names) > 0:      
+      print >> sys.stderr, "Your query string has some column names that don't " + \
+         "appear in the slog file: " + ",".join(unmatched_names)
+      print_help_and_exit()
    
 #===============================================================================
 
@@ -157,60 +332,72 @@ def parse_one_line(s, var_name_to_col_number):
    
 #===============================================================================
 
+def line_meets_select_criteria(line_dict, query_string, valid_col_names):
+   query_split = split_query_string(query_string)
+   query_col_names = find_col_names_in_query(query_string, query_split, valid_col_names)
+   
+   # We convert 'NaN' values to None, because it will probably lead to about the
+   # expected behavior in terms of how the query works...
+   
+   for (split_idx, col_name) in query_col_names.iteritems():
+      col_value = line_dict[col_name]
+      if col_value == "NaN":
+         query_split[split_idx] = "None"
+      else:
+         query_split[split_idx] = col_value
+   
+   bound_query = " ".join(query_split)
+   
+   try:
+      return eval(bound_query)
+   except Exception, inst:   
+      print >> sys.stderr, "Problem evaluating query string:\n" + \
+         "    " + bound_query + "\n\n"
+         
+      sys.exit(str(inst))
+
+#===============================================================================
+
 def main(argv):
-   args = parse_cmdline_args(argv)
-   f = open(args['input-filename'])
    
-   outfile = args['outfile']
+   f = get_input_file(argv)
+   outfile = get_output_file(argv)   
    
-   #----------------------------------------------------------------------------
-   # Copy the first 5 lines of the input file, verbatim...
-   #----------------------------------------------------------------------------
+   # Copy the first 5 lines of the input file, verbatim.  Actually, we'll print
+   # these a little later, after we've completed validating the command-line
+   # parms.  Otherwise it will look odd for this script to print the first part
+   # of an slog file and *then* complain about the command-line args...
+   first_header_lines = ""
    for i in range(5):
-      s = f.readline()
-      print >> outfile, s.strip()
+      first_header_lines = first_header_lines + f.readline().strip() + "\n"
    
+   # OK, NOW we can parse the header
    var_name_to_col_number = parse_slog_header(f)
+   valid_col_names = var_name_to_col_number.keys()
    
-   #----------------------------------------------------------------------------
-   # Confirm that the variables listed on the command line are a subset of the
-   # variables present in the slog file.  Avoid using Python 'sets' for this,
-   # because Mike's computer still runs, I believe, Python 2.4...
-   #----------------------------------------------------------------------------
-   if 'vars' in args:
-      user_specified_cols = args['vars']
+   # We needed to parse the header so we can determine the output columns.
+   output_vars = get_output_vars_list(argv, var_name_to_col_number)
+   
+   # If the user gave an explicit list of output variables, but left out TIME,
+   # assume it was a mistake...
+   if ('--vars' in argv) and ('TIME' not in output_vars):
+      output_vars = ['TIME'] + output_vars
+   
+   if len(output_vars) == 0:
+      sys.exit("No output columns selected, so nothing to do.")
+   
+   if "--select" in argv:
+      query_string = get_query_expression(argv, var_name_to_col_number)
+      validate_query_string(query_string, var_name_to_col_number.keys())
    else:
-      user_specified_cols = args['hidevars']
+      query_string = None
    
-   missing_vars = []
-   for a_var in user_specified_cols:
-      if a_var not in var_name_to_col_number:
-         missing_vars.append(a_var)
-         
-   if len(missing_vars) > 0:
-      sys.exit("You listed the following vars, but " + \
-         "the input file didn't have them: " + str(missing_vars))
-   
-   #----------------------------------------------------------------------------         
-   # Determine the exact set of columns to output...
-   #----------------------------------------------------------------------------
-   if 'vars' in args:
-      output_vars = args['vars']
-      if 'TIME' not in output_vars:
-         output_vars = ['TIME'] + output_vars
-   else:
-      # Preserve the order of the columns from the original file.
-      # Yes, I know this is an O(n^2) approach, but that's ok enough for now...
-      output_vars = []
-      for i in range(len(var_name_to_col_number)):
-         for (var_name, col_num) in var_name_to_col_number.iteritems():
-            if (col_num == i) and (var_name not in args['hidevars']):
-               output_vars.append(var_name)
-         
    #----------------------------------------------------------------------------
    # Print a reconstucted header to stdout.  Order the columns according to how
    # they were specified on the command-line in the "--vars" section...
    #----------------------------------------------------------------------------
+   print >> outfile, first_header_lines
+   
    for i in range(len(output_vars)):
       var_name = output_vars[i]
       print >> outfile, "%%   (" + str(i+1) + ") " + var_name
@@ -239,6 +426,11 @@ def main(argv):
       if (a_line != "") and (not a_line.startswith('%')):
          line_dict = parse_one_line(a_line, var_name_to_col_number)
          
+         if query_string != None:
+            if not line_meets_select_criteria(line_dict, query_string, valid_col_names):
+               a_line = f.readline()
+               continue
+         
          s = ""
          for i in range(len(output_vars)):
             var_name = output_vars[i]
@@ -254,5 +446,3 @@ def main(argv):
 
 if __name__ == '__main__':
    main(sys.argv)
-
-#===============================================================================
