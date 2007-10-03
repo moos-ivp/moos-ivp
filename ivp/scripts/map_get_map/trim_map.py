54376ad18b39336e770db57e68b206f19d02c0c4
Christian Convey <christian.convey@navy.mil>
2007-10-03 00:56:16 +0000

    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@298 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/scripts/map_get_map/trim_map.py
+++ ivp/scripts/map_get_map/trim_map.py
@@ -9,7 +9,10 @@ import os.path
 def print_usage():
    print \
 """
-Usage: trim_map.py  <infile>  [ -keep <bottom-lat> <top-lat>  <left-lon>  <right-lon> ] <x-size>  <y-size>  <outfile>
+Usage: trim_map.py  <infile>  <outfile>  \
+   -keep  <bottom-lat>  <top-lat>  <left-lon>  <right-lon>  \
+   -size <x-size>  <y-size> \
+   -origin <origin-lat>  <origin-lon>
    
 If the the '-keep' option and associated parameters are supplied, this trims the
 input image in a way that keeps the specified region.  If '-keep' and its 
@@ -27,14 +30,22 @@ requirement, the script will fail.)
 input image is smaller in either dimension than the output image is specified to
 be, this script will fail.
 
+<origin-lat> and <origin-lon> give the location of the origin of the (x,y)
+coordinate system used in the AUV software.  This script just needs this 
+information when writing the .info file.
+
 The script will produce two files: 
    
 <outfile> is the name of the .jpg file that this script will produce.  
-This script will *not* add a .jpg suffix to the filename; you need to specify 
+This script will *not* add a .tif suffix to the filename; you need to specify 
 that.  Outfile will be exactly 2048 x 2048 pixels in size.
 
-<outfile.info> describes the details of the output file, suitable for use in 
-pViewer.
+Note that the image format of the output file is dictacted by the filename
+extension you use for <outfile>.  So, for example, if you specify 
+"foo.tif", your output file will be a TIFF file.
+
+<out-info-file> describes the details of the output file, suitable for use in 
+polyview.  It will presumably be named something like "foo.info".
 """
 
 #===============================================================================
@@ -59,10 +70,11 @@ def get_image_xy_size(filename):
 
 #===============================================================================
 
-def crop_image(input_filename, output_filename, new_x_pixels, new_y_pixels):
+def crop_image(input_filename, output_filename, new_x_pixels, new_y_pixels, crop_x_offset, crop_y_offset):
    cmd = ['convert',  
       '-gravity', 'SouthWest',
-      '-crop',  str(new_x_pixels) + 'x' + str(new_y_pixels) + '+0+0',
+      '-crop',  str(new_x_pixels) + 'x' + str(new_y_pixels) \
+         + '+' + str(crop_x_offset) + '+' + str(crop_y_offset),
       input_filename, 
       output_filename]
    
@@ -98,98 +110,146 @@ def parse_input_img_filename(filename):
 #===============================================================================
 
 def main(argv):
-   if '-keep' in argv:
-      if len(argv) != 10:
-         print_usage();
-         sys.exit(1);
+   if (len(argv) != 14) \
+         or (argv[3] != '-keep') \
+         or (argv[8] != '-size') \
+         or (argv[11] != '-origin'):
+      print_usage();
+      sys.exit(1);
+      
+   try:
+      infile         = argv[1]
+      outfile        = argv[2]
+      desired_blat   = float(argv[4])
+      desired_tlat   = float(argv[5])
+      desired_llon   = float(argv[6])
+      desired_rlon   = float(argv[7])
+      desired_x_size = int(argv[9])
+      desired_y_size = int(argv[10])
+      origin_lat     = float(argv[12])
+      origin_lon     = float(argv[13])
+   except:
+      print sys.exc_info()
+      print "\n\n"
+      print_usage()
+      sys.exit(1)
+   
+   # Confirm that the user isn't asking to grow the image...
+   (input_image_x, input_image_y) = get_image_xy_size(infile)
+   if (desired_x_size > input_image_x) or (desired_y_size > input_image_y):
+      sys.exit("You specified an output image of width, height=" + \
+         str(desired_x_size) + ", " + str(desired_y_size) + "\n" + \
+         "But the input image has width, height of " + \
+         str(input_image_x) + ", " + str(input_image_y) + "\n" + \
+         "The output image cannot be bigger on either axis than the input image.")
+   
+   # Confirm that the desired image actually lies within the input image.
+   # Note that this probably only works for images that don't span the poles,
+   # prime meridian, etc.
+   #
+   # We also assume that the image is oriented with the increasing-y axis 
+   # pointing north.  This assures us that the bottom latitude is < the top
+   # latitude (as long as the image doesn't span a pole of the earth), and 
+   # the left longitude is less than the right longitude (as long as the 
+   # image doesn't cross the prime meridian)...
+   (input_blat, input_tlat, input_llon, input_rlon) = parse_input_img_filename(infile)
+   
+   if ((desired_blat < input_blat) or (desired_blat > input_tlat)):
+      sys.exit("Problem: <bottom-lat> is outside the latitude range of the input file.")
+   
+   if ((desired_tlat < input_blat) or (desired_tlat > input_tlat)):
+      sys.exit("Problem: <top-lat> is outside the latitude range of the input file.")
+      
+   if (desired_blat > desired_tlat):
+      sys.exit("Problem: <bottom-lat> is greater than <top-lat>.")
          
-      try:
-         infile         = argv[1]
-         desired_blat   = float(argv[3])
-         desired_tlat   = float(argv[4])
-         desired_llon   = float(argv[5])
-         desired_rlon   = float(argv[6])
-         desired_x_size = int(argv[7])
-         desired_y_size = int(argv[8])
-         outfile        = argv[9]
-      except:
-         print sys.exc_info()
-         print "\n\n"
-         print_usage()
-         sys.exit(1)
+   
+   if ((desired_llon < input_llon) or (desired_llon > input_rlon)):
+      sys.exit("Problem: <left-lon> is outside the longitude range of the input file.")
+   
+   if ((desired_rlon < input_llon) or (desired_rlon > input_rlon)):
+      sys.exit("Problem: <right-lon> is outside the longitude range of the input file.")
       
-      # Confirm that the user isn't asking to grow the image...
-      (input_image_x, input_image_y) = get_image_xy_size(infile)
-      if (desired_x_size > input_image_x) or (desired_y_size > input_image_y):
-         sys.exit("You specified an output image of width, height=" + \
-            str(desired_x_size) + ", " + str(desired_y_size) + "\n" + \
-            "But the input image has width, height of " + \
-            str(input_image_x) + ", " + str(input_image_y) + "\n" + \
-            "The output image cannot be bigger on either axis than the input image.")
+   if (desired_llon > desired_rlon):
+      sys.exit("Problem: <left-lon> is greater than <right-lon>.")
       
-      # Confirm that the desired image actually lies within the input image.
-      # Note that this probably only works for images that don't span the poles,
-      # prime meridian, etc.
-      #
-      # We also assume that the image is oriented with the increasing-y axis 
-      # pointing north.  This assures us that the bottom latitude is < the top
-      # latitude (as long as the image doesn't span a pole of the earth), and 
-      # the left longitude is less than the right longitude (as long as the 
-      # image doesn't cross the prime meridian)...
-      (input_blat, input_tlat, input_llon, input_rlon) = parse_input_img_filename(infile)
-      
-      if ((desired_blat < input_blat) or (desired_blat > input_tlat)):
-         sys.exit("Problem: <bottom-lat> is outside the latitude range of the input file.")
-      
-      if ((desired_tlat < input_blat) or (desired_tlat > input_tlat)):
-         sys.exit("Problem: <top-lat> is outside the latitude range of the input file.")
-         
-      if (desired_blat > desired_tlat):
-         sys.exit("Problem: <bottom-lat> is greater than <top-lat>.")
+   # Figure out the relationship between pixels and lat/lon.
+   #
+   # This is only an approximation, because lines of longitude aren't 
+   # parallel.  But it's safe enough at the scales we work at...
+   input_lat_span_degrees = input_tlat - input_blat
+   input_lon_span_degrees = input_rlon - input_llon
+   
+   desired_lat_span_degrees = desired_tlat - desired_blat
+   desired_lon_span_degrees = desired_tlat - desired_blat
+   
+   input_lat_degrees_per_pixel = input_lat_span_degrees / input_image_y
+   input_lon_degrees_per_pixel = input_lon_span_degrees / input_image_x
+   
+   required_output_image_y = int(input_image_y * (desired_lat_span_degrees / input_lat_span_degrees))
+   required_output_image_x = int(input_image_x * (desired_lon_span_degrees / input_lon_span_degrees))
+   
+   if (required_output_image_y > desired_y_size) or (required_output_image_x > desired_x_size):
+      sys.exit( \
+         "Problem: In order to produce an output image that covers the specified \n" + \
+         "   lat/lon range, the output image would need to have width,height = " + \
+         str(required_output_image_x) + "," + str(required_output_image_y) + "\n\n" + \
+         "   That's bigger than you specified the output image size to be.")
+
+   # Figure out where in the input and output files the origin is.  We really
+   # only need to know it for the output file, but just to be helpful we'll 
+   # report where it is in the input file.
+   input_origin_x_fraction = (origin_lon - input_llon) / input_lon_span_degrees
+   input_origin_y_fraction = (origin_lat - input_blat) / input_lat_span_degrees
+
+   # Figure out what pixel offset (both x- and y-) into the original image
+   # should be the bottom-left corner of the content that ends up in the
+   # output image...
+   crop_x_offset = int((desired_blat - input_blat) / input_lat_degrees_per_pixel)
+   crop_y_offset = int((desired_llon - input_llon) / input_lon_degrees_per_pixel)
             
-      
-      if ((desired_llon < input_llon) or (desired_llon > input_rlon)):
-         sys.exit("Problem: <left-lon> is outside the longitude range of the input file.")
-      
-      if ((desired_rlon < input_llon) or (desired_rlon > input_rlon)):
-         sys.exit("Problem: <right-lon> is outside the longitude range of the input file.")
-         
-      if (desired_llon > desired_rlon):
-         sys.exit("Problem: <left-lon> is greater than <right-lon>.")
-         
-      # Figure out the relationship between pixels and lat/lon.
-      #
-      # This is only an approximation, because lines of longitude aren't 
-      # parallel.  But it's safe enough at the scales we work at...
-      input_lat_span_degrees = input_tlat - input_blat
-      input_lon_span_degrees = input_rlon - input_llon
-      
-      desired_lat_span_degrees = desired_tlat - desired_blat
-      desired_lon_span_degrees = desired_tlat - desired_blat
-      
-      input_lat_degrees_per_pixel = input_lat_span_degrees / input_image_y
-      input_lon_degrees_per_pixel = input_lon_span_degrees / input_image_x
-      
-      print "Input image:"
-      print "   lat degrees per pixel: " + str(input_lat_degrees_per_pixel)
-      print "   lon degrees per pixel: " + str(input_lon_degrees_per_pixel)
-      
-      required_output_image_y = int(input_image_y * (desired_lat_span_degrees / input_lat_span_degrees))
-      required_output_image_x = int(input_image_x * (desired_lon_span_degrees / input_lon_span_degrees))
-      
-      if (required_output_image_y > desired_y_size) or (required_output_image_x > desired_x_size):
-         sys.exit( \
-            "Problem: In order to produce an output image that covers the specified \n" + \
-            "   lat/lon range, the output image would need to have width,height = " + \
-            str(required_output_image_x) + "," + str(required_output_image_y) + "\n\n" + \
-            "   That's bigger than you specified the output image size to be.")
-             
-      crop_image(infile, outfile, desired_x_size, desired_y_size)
-      
-      
-   else:
-      #TODO
-      pass
+   # Figure out the lat/lon bounds of the *actual* output image being produced...                        
+   outfile_llon = input_llon + (crop_x_offset * input_lon_degrees_per_pixel)
+   outfile_rlon = outfile_llon + (required_output_image_x * input_lon_degrees_per_pixel)            
+   outfile_blat = input_blat + (crop_x_offset * input_lat_degrees_per_pixel)
+   outfile_tlat = outfile_blat + (required_output_image_y * input_lon_degrees_per_pixel)
+   
+   outfile_lon_span_degrees = outfile_rlon - outfile_llon
+   outfile_lat_span_degrees = outfile_tlat - outfile_blat
+   
+   # Figure out where in the output image the origin is, but fractional and 
+   # pixel offset...
+   outfile_origin_x_fraction = (origin_lon - outfile_llon) / outfile_lon_span_degrees
+   outfile_origin_y_fraction = (origin_lat - outfile_blat) / outfile_lat_span_degrees
+   
+   outfile_origin_x_pixels = int(outfile_origin_x_fraction * required_output_image_x)
+   outfile_origin_y_pixels = int(outfile_origin_y_fraction * required_output_image_y)
+   
+   
+   print "Input image:"
+   print "   lat degrees per pixel: " + str(input_lat_degrees_per_pixel)
+   print "   lon degrees per pixel: " + str(input_lon_degrees_per_pixel)
+   print "   origin (fraction into image): x-axis=" + str(input_origin_x_fraction) \
+      + ", y-axis=" + str(input_origin_y_fraction)
+   
+   print "\n"
+   print "Output image:"
+   print "   Size (pixels) needed to contain specified lat/lon region: " + \
+      str(required_output_image_x) + " wide, " + str(required_output_image_y) + " high"
+   print "   Bottom-left of output image is x=" + str(crop_x_offset) + \
+      ", y=" + str(crop_y_offset) + " pixels from bottom-left of input image."
+   print "   Lat/lon bounds:"
+   print "      lat=[" + str(outfile_blat) + ", " + str(outfile_tlat) + "]"
+   print "      lon=[" + str(outfile_llon) + ", " + str(outfile_rlon) + "]"
+   print "   Origin:"
+   print "      fraction into image: x-axis=" + str(outfile_origin_x_fraction) \
+      + ", y-axis=" + str(outfile_origin_y_fraction)
+   print "      pixel offset relative to bottom-left corner: x=" + str(outfile_origin_x_pixels) \
+      + ", y=" + str(outfile_origin_y_pixels)
+   print ""            
+            
+   crop_image(infile, outfile, desired_x_size, desired_y_size, crop_x_offset, crop_y_offset)
+
 
 #===============================================================================
 
