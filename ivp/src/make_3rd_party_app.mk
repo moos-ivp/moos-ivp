94e5bbda87ac89fa7d78027fdbb7ae390241a3e1
Christian Convey <christian.convey@navy.mil>
2007-10-22 20:13:58 +0000

    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@375 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

---
+++ ivp/src/make_3rd_party_app.mk
@@ -0,0 +1,58 @@
+# Generic makefile structure for all application modules
+.SILENT:
+
+MOOS_LFLAGS = \
+	-L ../../../../MOOS/MOOSBin
+
+MOOS_IFLAGS = \
+	-I ../../../../MOOS/Essentials \
+	-I ../../../../MOOS/Core \
+	-I ../../../../MOOS/NavigationAndControl \
+	-I ../../../../MOOS/Thirdparty/FLTKVW \
+	-I ../../../../MOOS/Essentials/MOOSUtilityLib \
+	-I ../../../../MOOS/Core/MOOSLIB \
+	-I ../../../../MOOS/Core/MOOSGenLib \
+	-I ../../../../MOOS/NavigationAndControl\MOOSNavLib \
+	-I ../../../../MOOS/NavigationAndControl\MOOSTaskLib \
+	-I ../../../../MOOS/Thirdparty/FLTKVW
+
+
+BIN_DIR  = ../../bin
+TARGET   = $(BIN_DIR)/$(LTARGET)
+IFLAGS   = -I. -I.. -I../.. $(patsubst %,-I%,$(PLDIRS)) $(MOOS_IFLAGS)
+
+
+LFLAGS   = -L/usr/X11R6/lib -L/usr/X11R6/lib64     \
+	   -L/sw/lib $(patsubst %,-L%,$(PLDIRS))   \
+	   $(MOOS_LFLAGS)
+           
+
+SOURCES  = $(wildcard *.cpp)
+OBJECTS  = $(SOURCES:.cpp=.o)
+CFLAGS   = $(DEBUG_LEVEL) -Wno-deprecated  $(FP) $(LCFLAGS)
+
+vpath %.a  $(PLDIRS) 
+
+all:  	$(TARGET)
+	makedepend > /dev/null 2>&1 ; rm -f *akefile.bak
+
+depend:
+	makedepend $(IFLAGS) $(SOURCES) > /dev/null 2>&1
+
+clean:
+	@rm -f $(TARGET) $(LTARGET)
+	makedepend > /dev/null 2>&1
+	@rm -f *.bak *~ $(OBJECTS)
+	@echo Finished cleaning $(LTARGET)
+
+$(TARGET): $(OBJECTS) $(PLIBS)
+	@echo "Linking now....." $(LTARGET) 
+	$(CXX) $(CFLAGS) -o $(TARGET) $(OBJECTS) $(IFLAGS) $(LFLAGS) $(LIBS) 
+	@chmod 777 $(TARGET)
+
+%.o: %.cpp
+	@echo $(CXX) "compiling" $(LTARGET) "source code" $<
+	@echo "IFLAGS: " $(IFLAGS)
+	$(CXX) $(CFLAGS) $(IFLAGS) -c $<
+
+# DO NOT DELETE THIS LINE -- make depend depends on it.
