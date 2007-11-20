431f8456ed8f49529143393fcb2e1e933cab4b1a
Christian Convey <christian.convey@navy.mil>
2007-11-20 02:52:34 +0000

    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@474 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/scripts/slog-to-taml/slog-to-taml.py
+++ ivp/scripts/slog-to-taml/slog-to-taml.py
@@ -9,8 +9,70 @@ def print_usage_and_exit():
 
 #===============================================================================
 
+def emit_file_header(taml_file):
+   print >> taml_file, 
+"""   
+<TAML xmlns="urn:us:gov:dod:don:navy:navsea:usw:2:0">
+      <Operation xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:udt="urn:us:gov:dod:don:enterprise:udt:1:0" xmlns:usw="urn:us:gov:dod:don:navy:navsea:usw:2:0" xmlns:qdt="urn:us:gov:dod:don:navy:navsea:usw:qdt:1:0">
+               <Event>
+                     <StartTime>2006-09-12T10:38:49</StartTime>
+               </Event>
+      </Operation>
+      <Platform xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:udt="urn:us:gov:dod:don:enterprise:udt:1:0" xmlns:usw="urn:us:gov:dod:don:navy:navsea:usw:2:0" xmlns:qdt="urn:us:gov:dod:don:navy:navsea:usw:qdt:1:0">
+               <Name>USVField_1_1</Name>
+               <GUID>USVField_1_1</GUID>
+               <ShipClass>USV</ShipClass>
+               <ShipType>USV</ShipType>
+               <HullNumber>hullnum</HullNumber>
+               <Track>
+"""
+#===============================================================================
+
+def emit_position_report(taml_file, slog_timestamp, lat, lon, heading):
+   
+   # TODO: Compute DTG...
+   dtg = timestamp
+   
+   print >> taml_file, 
+"""
+                  <TimePosition>
+                           <DateTimeGroup>
+                                    <DateTime>%s</DateTime>
+                           </DateTimeGroup>
+                           <Position>
+                                    <AbsolutePosition>
+                                          <Latitude>%s</Latitude>
+                                          <Longitude>%s</Longitude>
+                                          <Altitude>0</Altitude>
+                                    </AbsolutePosition>
+                           </Position>
+                           <Orientation>
+                                    <Heading>%s</Heading>
+                                    <Pitch>0</Pitch>
+                                    <Roll>0</Roll>
+                           </Orientation>
+                           <Speed>0</Speed>
+                  </TimePosition>
+""" % (dtg, str(lat), str(lon), str(heading))
+
+#===============================================================================
+
+def emit_file_footer(taml_file):
+   print >> taml_file, 
+"""
+		</Track>
+	</Platform>	
+</TAML>
+"""
+
+#===============================================================================
+
 def slog_to_taml(slog_file, taml_file):
-   pass
+   emit_file_header(taml_file)
+   
+   emit_position_report(taml_file, lat, lon, heading)
+   
+   emit_file_footer(taml_file)
 
 #===============================================================================
 
