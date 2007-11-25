3eb83dd117828e8f7de86e6c85a831ac1744e590
Mike Benjamin <mikerb@csail.mit.edu>
2007-11-25 22:10:43 +0000

    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@503 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/src/uProcessWatch/main.cpp
+++ ivp/src/uProcessWatch/main.cpp
@@ -1,8 +1,23 @@
 /*****************************************************************/
-/*    NAME: Michael Benjamin                                     */
+/*    NAME: Michael Benjamin and John Leonard                    */
 /*    ORGN: NAVSEA Newport RI and MIT Cambridge MA               */
 /*    FILE: main.cpp                                             */
-/*    DATE: May 27th 2007  (MINUS-07)                            */
+/*    DATE: May 27th 2007 (MINUS-07)                             */
+/*                                                               */
+/* This program is free software; you can redistribute it and/or */
+/* modify it under the terms of the GNU General Public License   */
+/* as published by the Free Software Foundation; either version  */
+/* 2 of the License, or (at your option) any later version.      */
+/*                                                               */
+/* This program is distributed in the hope that it will be       */
+/* useful, but WITHOUT ANY WARRANTY; without even the implied    */
+/* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR       */
+/* PURPOSE. See the GNU General Public License for more details. */
+/*                                                               */
+/* You should have received a copy of the GNU General Public     */
+/* License along with this program; if not, write to the Free    */
+/* Software Foundation, Inc., 59 Temple Place - Suite 330,       */
+/* Boston, MA 02111-1307, USA.                                   */
 /*****************************************************************/
 
 #include "MOOSLib.h"
@@ -22,4 +37,5 @@ int main(int argc, char *argv[])
   
   return(0);
 }
+
 
