8077c456fde9c86c09c80afd48ce538e7417929d
Andrew Shafer <ajshafer@mit.edu>
2007-11-27 22:20:05 +0000

    Added snap parameter.
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@514 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/src/3rd-party-ajshafer/app_generatelawnmower/generatelawnmowerMain.cpp
+++ ivp/src/3rd-party-ajshafer/app_generatelawnmower/generatelawnmowerMain.cpp
@@ -8,27 +8,71 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <fstream>
 #include "MBUtils.h"
 #include "XYPolygon.h"
 #include "XYSegList.h"
 #include "IO_GeomUtils.h"
 #include "ArtifactUtils.h"
+#include "AngleUtils.h"
 
 void help_message();
 
 int main(int argc, char *argv[])
 {
 	// argv[0] = program name
-	// argv[1] = file to read polygons from
-	// argv[2] = file to put poly/seglist into
+	// argv[1] = x
+	// argv[2] = y
+	// argv[3] = angle
+	// argv[4] = radius
+	// argv[5] = clockwise (true/false)
+	// argv[6] = file to read polygons from
+	// argv[7] = [snap value]
 	// Read in the polygons
 	// generate lawnmower pattern
 	// Output to files
-	if (argc != 2){ help_message(); return 1; };
+	if (argc != 8 && argc != 7 && argc != 2 && argc != 3){ help_message(); return 1; };
 	
+	double px0 = 0, py0 = 0, ang = 90, radius = 10, snap = 0.0;
+	bool clockwise = true;
+	std::string spx0 = "0", spy0 = "0", sang = "90", sradius = "10", sclockwise = "TRUE", ssnap = "0";
+	std::string ofHeader;
 	
-	std::vector<XYPolygon> vecPolys = readPolysFromFile(argv[1]);
+    if (argc == 2) { ofHeader = argv[1]; }
+    else if (argc == 3) { ofHeader = argv[1]; ssnap = argv[2]; }
+    else if (argc == 7 || argc == 8) {
+    	spx0 = argv[1];
+    	spy0 = argv[2];
+    	sang = argv[3];
+    	sradius = argv[4];
+    	sclockwise = argv[5];
+    	ofHeader = argv[6];
+    	if (argc == 7) {ssnap = argv[7];}
+    }
 	
+	if (toupper(sclockwise) == "TRUE") {clockwise = true;}
+	else if (toupper(sclockwise) == "FALSE") {clockwise = false;}
+	
+	bool ok = isNumber(spx0) && isNumber(spy0) && isNumber(sang) && isNumber(sradius) && isNumber(ssnap);
+	if (ok) {
+		px0 = atof(spx0.c_str());
+		py0 = atof(spy0.c_str());
+		ang = angle360(atof(sang.c_str()));
+		radius = atof(sradius.c_str());
+		snap = atof(ssnap.c_str());
+	}
+	else { help_message(); return 1; }
+	
+	std::ofstream ofPolys, ofSegLists;
+	std::string ofPolyName = ofHeader;
+			ofPolyName.append("_polys");
+	std::string ofSegListName = ofHeader;
+			ofSegListName.append("_seglists");
+	
+	ofPolys.open(ofPolyName.c_str());
+	ofSegLists.open(ofSegListName.c_str());
+	
+	std::vector<XYPolygon> vecPolys = readPolysFromFile(ofHeader);
 	std::vector<XYSegList> vecMowers;
 	
 	std::vector<XYPolygon>::iterator p;
@@ -37,12 +81,25 @@ int main(int argc, char *argv[])
 		std::cout << p->get_spec() << std::endl;
 		
 		//generateLawnmower(poly, x0, y0, angle, radius, clockwise?);
-		vecMowers.push_back(generateLawnmower(*p, 0, 0, 45, 10, true));
+		//std::cout << "Calling with: " << px0 << py0 << ang << radius << clockwise << std::endl;;
+		XYSegList mower = generateLawnmower(*p, px0, py0, ang, radius, clockwise);
+		mower.set_label(p->get_label());
+		mower.apply_snap(snap);
+		vecMowers.push_back(mower);
 		
 		std::cout << "Got mower pattern: \n";
 		std::cout << vecMowers.at(vecMowers.size()-1).get_spec() << std::endl;
 		
+		// Write out to files
+		if (ofPolys.is_open() && ofSegLists.is_open()){
+			ofPolys << p->get_spec() << std::endl;
+			ofSegLists << vecMowers.at(vecMowers.size()-1).get_spec() << std::endl;
+		}
+		
 	};
+	
+	ofPolys.close();
+	ofSegLists.close();
 }
 
 
@@ -55,7 +112,8 @@ void help_message()
 
 	cout << endl;
 	cout << "Usage:  generatelawnmower" << endl;
-	cout << "generatelawnmower input_file" << endl;
+	cout << "generatelawnmower x y angle radius clockwise input_file [snap value]" << endl;
+	cout << "generatelawnmower input_file [snap value]" << endl;
 	cout << endl;
 }
 
