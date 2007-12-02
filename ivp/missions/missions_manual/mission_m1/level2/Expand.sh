2ad3177cb4cb1baf37c5cb086de8ca1805b58ee9
Mike Benjamin <mikerb@csail.mit.edu>
2007-12-02 20:14:35 +0000

    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@547 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/missions/missions_manual/mission_m1/level2/Expand.sh
+++ ivp/missions/missions_manual/mission_m1/level2/Expand.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-splug meta_vehicle1.moos ../vehicle1.moos
-splug meta_vehicle2.moos ../vehicle2.moos
+splug meta_vehicle.moos ../vehicle1.moos \
+    VID=1                                \
+    SIM_START_POS=10,-15,180,0
+
+splug meta_vehicle.moos ../vehicle2.moos \
+    VID=2                                \
+    SIM_START_POS=-10,-25,180,0
 
