8f288a2c953e70f358527e11b7507f681280568c
Doug Maxwell <doug.maxwell@navy.mil>
2007-12-05 21:09:55 +0000

    1.  reduced m_rotation_increment to 1.0f 2.  Set initial values of camera angles to zero 3.  Encapsulated the three draw calls within one push/pop matrix.  Will probably be changed later. 4.  Made kayak placement a passable parameter, not hard coded. 5.  Removed setCameraYaw, incCameraYaw, setCameraPitch, incCameraPitch 6.  created new procedure setCameraAngle, which deals with all axis simultaneously. 7.  added setCOI (center of interest) procedure.  currently broken 8.  added translateCamera procedure
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@560 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/src/app_wireframe_viewer/WireframeViewer.cpp
+++ ivp/src/app_wireframe_viewer/WireframeViewer.cpp
@@ -14,6 +14,8 @@
 #include <GL/glu.h>
 #include "WireframeViewer.h"
 
+#define DEG2RAD 0.01745329
+
 //-----------------------------------------------------------
 // Constructor
 
@@ -23,14 +25,14 @@ WireframeViewer::WireframeViewer(int x, int y, int w, int h, char* title)
   mode(FL_RGB | FL_ALPHA | FL_DEPTH | FL_DOUBLE);
 
   m_rotation = 0.f;
-  m_rotation_increment = 10.f;
+  m_rotation_increment = 1.0f;
   m_animating = false;
   
   m_camera_x = 0;
-  m_camera_y = 0;
-  m_camera_z = 0;
+  m_camera_y = 2;
+  m_camera_z = 4;
 
-  m_camera_pitch = -20;
+  m_camera_pitch = 0;
   m_camera_yaw   = 0;
 
   m_target_x = 0;
@@ -38,7 +40,7 @@ WireframeViewer::WireframeViewer(int x, int y, int w, int h, char* title)
   m_target_z = 0;
 
   m_view_mode = 0;
-  m_zoom      = 1;
+  m_zoom      = 0;
 }
 
 //-----------------------------------------------------------
@@ -84,19 +86,17 @@ void WireframeViewer::draw(){
   glFrustum(-1, 1, -1, 1, 1, 120);  // I like 120 deg, your mileage may vary
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
-  gluLookAt(0,2,4,0,0,0,0,1,0);
+  gluLookAt(0,0,4,0,0,0,0,1,0); 
+  //gluLookAt(0,2,4,m_target_x,m_target_y,m_target_z,0,1,0); 
+  //setCOI(m_target_x,m_target_y,m_target_z);
+  setCameraAngle(); //setup ego centric viewing
   float lightPosition[4] = {5, 5, 5, 1};
   glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
-  glRotatef(m_rotation, 0, 1, 0);
-  
+  glPushMatrix();
+  glRotatef(m_rotation, 0, 1, 0);  // exo centric x/z planar rotation
   DrawAxes();
-  glPushMatrix();
-  glTranslatef(2.0, 0.0, 2.0);
-  DrawKayak();
-  glPopMatrix();
-  glPushMatrix();
-  glTranslatef(-2.0, 0.0, -2.0);
-  DrawKayak();
+  DrawKayak(4.0, 0.0, 2.0);
+  DrawKayak(-2.0, 0.0, -2.0);
   glPopMatrix();
 }
 
@@ -106,6 +106,7 @@ void WireframeViewer::draw(){
 void WireframeViewer::DrawAxes()
 {
   //visual aid, RED_X, GREEN_Y, BLUE_Z
+  //glPushMatrix();
   glBegin(GL_LINES);
   glColor3f(1,0,0);
   glVertex3f(-25.0,0,0);
@@ -121,13 +122,16 @@ void WireframeViewer::DrawAxes()
   glVertex3f(0,0,-25);
   glVertex3f(0,0,25);
   glEnd();
+  //glPopMatrix();
 }
 
 //-----------------------------------------------------------
 // Procedure: drawKayak
 
-void WireframeViewer::DrawKayak()
+void WireframeViewer::DrawKayak(float m_kayak_x, float m_kayak_y, float m_kayak_z)
 {
+  //glPushMatrix();
+  glTranslatef(m_kayak_x, m_kayak_y, m_kayak_z);
   glBegin(GL_LINE_LOOP);
   glColor3f(1,1,0);
   glVertex3f(-0.5,0,0);//central member
@@ -189,53 +193,69 @@ void WireframeViewer::DrawKayak()
   glVertex3f(0.125, -0.125, 1.25);
   glVertex3f(0.0625, -0.0625, 2.5);
   glEnd();	
+  //glPopMatrix();
 }
 
-//-----------------------------------------------------------
-// Procedure: setCameraYaw
 
-void WireframeViewer::setCameraYaw(float yaw)
+//-----------------------------------------------------------
+// Procedure: setCameraAngle
+
+void WireframeViewer::setCameraAngle()
 {
-  m_camera_yaw = yaw;
+  while(m_camera_pitch < 0)
+    m_camera_pitch += 360.0;
+  while(m_camera_pitch > 360.0)
+    m_camera_pitch -= 360;
+
   while(m_camera_yaw < 0)
     m_camera_yaw += 360.0;
   while(m_camera_yaw > 360.0)
-    m_camera_yaw -= 360;
+    m_camera_yaw -= 360; 
+
+  glRotatef(m_camera_yaw, 0.0, 1.0, 0.0);
+  glRotatef(m_camera_pitch, 1.0, 0.0, 0.0);
+  glTranslatef(-m_camera_x, -m_camera_y, -m_camera_z);
 }
 
 //-----------------------------------------------------------
-// Procedure: incCameraYaw
+// Procedure: setCOI
 
-void WireframeViewer::incCameraYaw(float amt)
+void WireframeViewer::setCOI(float tx, float ty, float tz)
 {
-  m_camera_yaw += amt;
-  while(m_camera_yaw < 0)
-    m_camera_yaw += 360.0;
-  while(m_camera_yaw > 360.0)
-    m_camera_yaw -= 360;
+   m_target_x = tx;
+   m_target_y = ty;
+   m_target_z = tz;
+
+   gluLookAt(m_camera_x, m_camera_y, m_camera_z,m_target_x,m_target_y,m_target_z,0,1,0); 
 }
 
 //-----------------------------------------------------------
-// Procedure: setCameraPitch
+// Procedure: reset
 
-void WireframeViewer::setCameraPitch(float pitch)
+void WireframeViewer::reset()
 {
-  m_camera_pitch = pitch;
-  while(m_camera_pitch < 0)
-    m_camera_pitch += 180.0;
-  while(m_camera_pitch > 180.0)
-    m_camera_pitch -= 180;
+   m_camera_x = 0; 
+   m_camera_y = 0; 
+   m_camera_z = 4; 
+   m_camera_pitch = 0; 
+   m_camera_yaw   = 0;
+
+   m_target_x = 0;
+   m_target_y = 0;
+   m_target_z = 0;
+
+   gluLookAt(0,0,4,m_target_x,m_target_y,m_target_z,0,1,0); 
 }
 
 //-----------------------------------------------------------
-// Procedure: incCameraPitch
+// Procedure: translate
 
-void WireframeViewer::incCameraPitch(float amt)
+void WireframeViewer::translateCamera(float cx, float cy, float cz)
 {
-  m_camera_pitch += amt;
-  while(m_camera_pitch < 0)
-    m_camera_pitch += 180.0;
-  while(m_camera_pitch > 180.0)
-    m_camera_pitch -= 180;
+   m_camera_x = cx;
+   m_camera_y = cy;
+   m_camera_z = cz;
+   glTranslatef(m_camera_x, m_camera_y, m_camera_z);
 }
+
 
