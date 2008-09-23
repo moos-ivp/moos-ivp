a763b159e6511f90a6d6b61684eea362d8f915cd
Mike Benjamin <mikerb@csail.mit.edu>
2008-09-23 18:38:07 +0000

    Removed OLD version of MOOS
    git-svn-id: svn+ssh://oceanai.mit.edu/home/conlanc/moos-ivp-full-mirror/trunk@1429 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- build-moos.sh
+++ build-moos.sh
@@ -29,10 +29,12 @@ cmake                                           \
    -DDB_HAS_HTTP=ON                             \
    -DBUILD_TOOLS=ON                             \
    -DBUILD_OCEAN_SIM=ON                         \
+   -DBUILD_NAVIGATION=OFF                       \
+   -DBUILD_HELM=OFF                             \
    -DBUILD_NEWMAT=OFF                           \
    -DBUILD_GRAPHICAL_TOOLS=ON                   \
-   -DBUILD_MATLAB_TOOLS=OFF                      \
-   -DMATLAB_DIR=/usr/local/matlab-2007a   \
+   -DBUILD_MATLAB_TOOLS=OFF                     \
+   -DMATLAB_DIR=/usr/local/matlab-2007a         \
    -DBUILD_LAND_INSTRUMENTS=OFF                 \
    -DBUILD_OCEAN_INSTRUMENTS=OFF                \
    -DBUILD_UMVS=ON                              \
