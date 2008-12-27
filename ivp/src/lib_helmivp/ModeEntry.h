0c7802f0557672cfe1c8528a24977f3f9c319611
Mike Benjamin <mikerb@csail.mit.edu>
2008-12-27 06:42:03 +0000

    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@1633 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

---
+++ ivp/src/lib_helmivp/ModeEntry.h
@@ -0,0 +1,44 @@
+/*****************************************************************/
+/*    NAME: Michael Benjamin and John Leonard                    */
+/*    ORGN: NAVSEA Newport RI and MIT Cambridge MA               */
+/*    FILE: ModeEntry.h                                          */
+/*    DATE: Dec 26th, 2008                                       */
+/*                                                               */
+/* This program is free software; you can redistribute it and/or */
+/* modify it under the terms of the GNU General Public License   */
+/* as published by the Free Software Foundation; either version  */
+/* 2 of the License, or (at your option) any later version.      */
+/*                                                               */
+/* This program is distributed in the hope that it will be       */
+/* useful, but WITHOUT ANY WARRANTY; without even the implied    */
+/* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR       */
+/* PURPOSE. See the GNU General Public License for more details. */
+/*                                                               */
+/* You should have received a copy of the GNU General Public     */
+/* License along with this program; if not, write to the Free    */
+/* Software Foundation, Inc., 59 Temple Place - Suite 330,       */
+/* Boston, MA 02111-1307, USA.                                   */
+/*****************************************************************/
+
+#ifndef MODE_ENTRY_HEADER
+#define MODE_ENTRY_HEADER
+
+#include <vector>
+#include <string>
+#include "IvPDomain.h"
+
+class ModeEntry {
+public:
+  ModeEntry() {};
+  ~ModeEntry() {};
+
+  void print();
+
+protected:
+  std::string  m_mode_var;
+  std::string  m_mode_val;
+  std::string  m_mode_val_else;
+  std::string  m_mode_condition;
+};
+
+#endif
