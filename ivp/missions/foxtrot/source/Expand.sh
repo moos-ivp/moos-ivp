1725b70e6e904a1e6bb675808fdc523ed2382519
Mike Benjamin <mikerb@csail.mit.edu>
2009-02-03 00:27:15 +0000

    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@1684 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

---
+++ ivp/missions/foxtrot/source/Expand.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+splug meta_vehicle.moos ../Henry.moos \
+    VID=1 VNAME=Henry                    \
+    SIM_START_POS=10,-15,180,0
+
+splug meta_vehicle.moos ../Tom.moos \
+    VID=2 VNAME=Tom                      \
+    SIM_START_POS=-10,-25,180,0
+
