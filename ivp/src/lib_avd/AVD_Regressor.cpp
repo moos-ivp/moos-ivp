f30f78aa0234af865423c417e1b922fe3693c4c0
Mike Benjamin <mikerb@csail.mit.edu>
2009-02-17 18:50:39 +0000

    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@1716 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/src/lib_avd/AVD_Regressor.cpp
+++ ivp/src/lib_avd/AVD_Regressor.cpp
@@ -20,6 +20,7 @@
 /* Boston, MA 02111-1307, USA.                                   */
 /*****************************************************************/
 
+#include <cstdlib>
 #include "AVD_Regressor.h"
 #include "MBUtils.h"
 
