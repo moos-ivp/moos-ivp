8be43d35b94cb9f8fded82d89955f71e63a216a7
Christian Convey <christian.convey@navy.mil>
2009-02-25 13:42:45 +0000

    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@1730 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- build-ivp.sh
+++ build-ivp.sh
@@ -3,6 +3,35 @@
 # Note by mikerb (Nov 20th 2008)
 #
 
-cd ivp/src/
-cmake ./
+INVOCATION_ABS_DIR="`pwd`"
+
+# We want absolute pathnames, which require a little extra work to get...
+SCRIPT_DIR="`dirname ${0}`"
+cd "${SCRIPT_DIR}"
+SCRIPT_ABS_DIR=`pwd`
+
+SRC_ABS_DIR="${SCRIPT_ABS_DIR}/ivp/src"
+TARGET_ABS_DIR="${INVOCATION_ABS_DIR}/ivp/src"
+
+if [ "${INVOCATION_ABS_DIR}" != "${SCRIPT_ABS_DIR}" ]; then
+   if [ -f "${SRC_ABS_DIR}/CMakeCache.txt" ]; then
+      echo ""
+      echo "The directory ${SRC_ABS_DIR} contains a file named CMakeCache.txt"
+      echo "You're trying to perform an out-of-source build, but that's "
+      echo "not possible if you have CMakeCache.txt files in your source "
+      echo "tree."
+      echo ""
+      echo "If you really want to do an out-of-source build, you should delete"
+      echo "all CMakeCache.txt files from your source tree."
+      echo ""
+      return 1
+   fi
+fi
+
+mkdir -p "${TARGET_ABS_DIR}"
+cd "${TARGET_ABS_DIR}"
+
+cmake "${SRC_ABS_DIR}"
 make
+
+cd ${INVOCATION_ABS_DIR}
--- build-moos.sh
+++ build-moos.sh
@@ -7,7 +7,34 @@
 # switching off of modules that are not typically built in 
 # local users of MOOS-IvP at MIT. 
 
-cd MOOS
+INVOCATION_ABS_DIR="`pwd`"
+
+# We want absolute pathnames, which require a little extra work to get...
+SCRIPT_DIR="`dirname ${0}`"
+cd "${SCRIPT_DIR}"
+SCRIPT_ABS_DIR=`pwd`
+
+SRC_ABS_DIR="${SCRIPT_ABS_DIR}/MOOS"
+TARGET_ABS_DIR="${INVOCATION_ABS_DIR}/MOOS"
+
+if [ "${INVOCATION_ABS_DIR}" != "${SCRIPT_ABS_DIR}" ]; then
+   if [ -f "${SRC_ABS_DIR}/CMakeCache.txt" ]; then
+      echo ""
+      echo "The directory ${SRC_ABS_DIR} contains a file named CMakeCache.txt"
+      echo "You're trying to perform an out-of-source build, but that's "
+      echo "not possible if you have CMakeCache.txt files in your source "
+      echo "tree."
+      echo ""
+      echo "If you really want to do an out-of-source build, you should delete"
+      echo "all CMakeCache.txt files from your source tree."
+      echo ""
+      return 1
+   fi
+fi
+
+mkdir -p "${TARGET_ABS_DIR}"
+cd "${TARGET_ABS_DIR}"
+
 cmake                                           \
    -DFLTK_DIR=/usr/lib                          \
    -DFLTK_INCLUDE_DIR=/usr/include              \
@@ -20,13 +47,15 @@ cmake                                           \
    -DBUILD_NAVIGATION=OFF                       \
    -DBUILD_HELM=OFF                             \
    -DBUILD_NEWMAT=OFF                           \
-   -DBUILD_GRAPHICAL_TOOLS=OFF                   \
+   -DBUILD_GRAPHICAL_TOOLS=OFF                  \
    -DBUILD_MATLAB_TOOLS=OFF                     \
    -DMATLAB_DIR=/usr/local/matlab-2007a         \
    -DBUILD_LAND_INSTRUMENTS=OFF                 \
    -DBUILD_OCEAN_INSTRUMENTS=OFF                \
    -DBUILD_UMVS=ON                              \
    -DBUILD_UPB=ON                               \
-   ./
+   "${SRC_ABS_DIR}"
 
 make
+
+cd ${INVOCATION_ABS_DIR}
