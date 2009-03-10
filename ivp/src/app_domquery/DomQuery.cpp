e945ec0faec210fa22d2d684aa10a64588e30310
Mike Benjamin <mikerb@csail.mit.edu>
2009-03-10 14:03:13 +0000

    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@1761 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/src/app_domquery/DomQuery.cpp
+++ ivp/src/app_domquery/DomQuery.cpp
@@ -32,19 +32,57 @@ DomQuery::DomQuery()
 void DomQuery::buildDomain()
 {
   vector<string> fvector = fileBuffer(m_infile);
-  int vsize = fvector.size();
+  unsigned int i, vsize = fvector.size();
   if(vsize == 0)
     return;
 
   IvPDomain new_domain;
   
   bool ok = true;
-  for(int i=0; ((i<vsize) && ok); i++) {
-    string line = fvector[i];
-    line = stripBlankEnds(line);
+  for(i=0; ((i<vsize) && ok); i++) {
+    string line = stripBlankEnds(fvector[i]);
     cout << line << endl;
+    bool var_set = false;
+    bool low_set = false;
+    bool hgh_set = false;
+    bool pts_set = false;
+    string var, low, hgh, pts;
+    vector<string> svector = parseString(line, ',');
+    unsigned int j, lsize = svector.size();
+    for(j=0; j<lsize; j++) {
+      svector[j] = stripBlankEnds(svector[j]);
+      vector<string> jvector = parseString(svector[j], '=');
+      if(jvector.size()==2) {
+	string left  = tolower(stripBlankEnds(jvector[0]));
+	string right = stripBlankEnds(jvector[1]);
+	if((left == "variable") || (left == "var")) {
+	  var_set = true;
+	  var = right;
+	}
+	if(left == "low") {
+	  low_set = true;
+	   low= right;
+	}
+	if((left == "hgh") || (left == "high")) {
+	  hgh_set = true;
+	  hgh = right;
+	}
+	if((left == "pts") || (left == "points")) {
+	  pts_set = true;
+	  pts = right;
+	}
+      }
+    }
+    if(var_set && low_set && hgh_set && pts_set &&
+       isNumber(low) && isNumber(hgh) && isNumber(pts)) {
+      double dlow = atof(low.c_str());
+      double dhgh = atof(hgh.c_str());
+      double dpts = atof(pts.c_str());
+      new_domain.addDomain(var, dlow, dhgh, dpts);
+    }
   }
 
+  new_domain.print();
 }
   
 //--------------------------------------------------------
