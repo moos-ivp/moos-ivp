c6255d82f1a75f4f5edd3ac7bf128855d0e79a60
Mike Benjamin <mikerb@csail.mit.edu>
2009-04-06 14:49:28 +0000

    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@1844 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/src/pEchoVar/EchoVar.cpp
+++ ivp/src/pEchoVar/EchoVar.cpp
@@ -20,6 +20,7 @@
 /* Boston, MA 02111-1307, USA.                                   */
 /*****************************************************************/
 
+#include <cstring>
 #include <iterator>
 #include "EchoVar.h"
 #include "MBUtils.h"
