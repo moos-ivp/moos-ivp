bd56f56a2ad2582e7ab743839c680fa3e22acc57
Mike Benjamin <mikerb@csail.mit.edu>
2009-04-24 18:40:41 +0000

    another minor change in dfault pmviewer size
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@1948 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/src/pMarineViewer/main_mviewer.cpp
+++ ivp/src/pMarineViewer/main_mviewer.cpp
@@ -186,9 +186,9 @@ int main(int argc, char *argv[])
   if(g_sMissionFile == 0)
     exit_with_usage();
   
-  //PMV_GUI* gui = new PMV_GUI(1100,880, "pMarineViewer");
+  PMV_GUI* gui = new PMV_GUI(1100,850, "pMarineViewer");
   //PMV_GUI* gui = new PMV_GUI(1100,700, "pMarineViewer");
-  PMV_GUI* gui = new PMV_GUI(1100,585, "pMarineViewer");
+  //PMV_GUI* gui = new PMV_GUI(1100,585, "pMarineViewer");
   if(!gui) {
     cout << "Unable to instantiate the GUI - exiting." << endl;
     return(-1);
