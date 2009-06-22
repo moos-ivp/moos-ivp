54bb6a6a8a3c183c1d4cbe7dbacf14a6540b31c8
Mike Benjamin <mikerb@csail.mit.edu>
2009-06-22 11:52:15 +0000

    added #include <cstdlib> to XYFormatUtilsCircle.cpp
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@2114 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/src/lib_geometry/XYFormatUtilsCircle.cpp
+++ ivp/src/lib_geometry/XYFormatUtilsCircle.cpp
@@ -20,6 +20,7 @@
 /* Boston, MA 02111-1307, USA.                                   */
 /*****************************************************************/
 
+#include <cstdlib>
 #include "MBUtils.h"
 #include "XYFormatUtilsCircle.h"
 
