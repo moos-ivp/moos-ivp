0b4c8bba97c3420eef6f07dc50688df7ce1da5c0
Mike Benjamin <mikerb@csail.mit.edu>
2009-08-17 18:46:02 +0000

    mods to uFunctionVis to reflect changes in dependent libs
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@2254 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/src/uFunctionVis/FV_Viewer.cpp
+++ ivp/src/uFunctionVis/FV_Viewer.cpp
@@ -15,18 +15,9 @@ using namespace std;
 FV_Viewer::FV_Viewer(int x, int y, int wid, int hgt, const char *l)
   : Common_IPFViewer(x, y, wid, hgt, l)
 {
-  m_model       = 0;
+  m_model = 0;
 
-  vector<double> cvect = colorParse("macbeige");
-  //vector<double> cvect = colorParse("macpurple");
-
-  //m_clear_red   =  73.0 / 256.0;
-  //m_clear_green =  62.0 / 256.0;
-  //m_clear_blue  = 120.0 / 256.0;
-
-  m_clear_red   = cvect[0];
-  m_clear_green = cvect[1];
-  m_clear_blue  = cvect[2];
+  m_clear_color.setColor("macbeige");
 }
 
 //-------------------------------------------------------------
