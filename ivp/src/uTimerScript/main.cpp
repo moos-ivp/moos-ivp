58da72555f588ad198dedaa24222cbe213386edc
Mike Benjamin <mikerb@csail.mit.edu>
2009-11-18 20:34:56 +0000

    mods to uTimerScript and release_notes.txt
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@2390 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/src/uTimerScript/main.cpp
+++ ivp/src/uTimerScript/main.cpp
@@ -6,20 +6,42 @@
 /************************************************************/
 
 #include <string>
+#include <vector>
 #include "MOOSLib.h"
 #include "MOOSGenLib.h"
 #include "TS_MOOSApp.h"
+#include "MBUtils.h"
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
-  std::string sMissionFile = "TimerScript.moos";
-	
-  if(argc > 1)
-    sMissionFile = argv[1];
-	
+  // Look for a request for version information
+  if(scanArgs(argc, argv, "-v", "--version", "-version")) {
+    vector<string> svector = getReleaseInfo("uTimerScript");
+    for(unsigned int j=0; j<svector.size(); j++)
+      cout << svector[j] << endl;    
+    return(0);
+  }
+
+  int    i;
+  string mission_file = "";
+  bool   help_requested = false;
+  for(i=1; i<argc; i++) {
+    string argi = argv[i];
+    if(strEnds(argi, ".moos"))
+      mission_file = argv[i];
+    if((argi == "--help")||(argi=="-h"))
+      help_requested = true;
+  }
+
+  if((mission_file == "") || help_requested) {
+    MOOSTrace("Usage: uTimerScript moosfile.moos \n");
+    return(0);
+  }
+
   TS_MOOSApp TimerScript;
-	
-  TimerScript.Run(argv[0], sMissionFile.c_str());
+  TimerScript.Run(argv[0], mission_file.c_str());
 
   return(0);
 }
