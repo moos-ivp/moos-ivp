87c6b271a0693672e55f32a164510667cd9ce54d
Mike Benjamin <mikerb@csail.mit.edu>
2009-11-18 18:13:26 +0000

    minor chg to uTimerScript --version
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/releases/moos-ivp-4.0@2387 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/src/uTimerScript/main.cpp
+++ ivp/src/uTimerScript/main.cpp
@@ -6,12 +6,24 @@
 /************************************************************/
 
 #include <string>
+#include <vector>
 #include "MOOSLib.h"
 #include "MOOSGenLib.h"
 #include "TS_MOOSApp.h"
+#include "MBUtils.h"
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
+  // Look for a request for version information
+  if(scanArgs(argc, argv, "-v", "--version", "-version")) {
+    vector<string> svector = getReleaseInfo("uTimerScript");
+    for(unsigned int j=0; j<svector.size(); j++)
+      cout << svector[j] << endl;    
+    return(0);
+  }
+
   std::string sMissionFile = "TimerScript.moos";
 	
   if(argc > 1)
