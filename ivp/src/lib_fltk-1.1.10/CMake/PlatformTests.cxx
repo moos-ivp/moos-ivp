18a1f34d76ed4fbbe773ff6654db24c5deee38e7
Chris Gagner <christopher.gagner@navy.mil>
2010-01-13 23:02:03 +0000

    Applied a patch to allow FLTK-1.1.10 to compile using CMake on computers with glibc version 2.10.
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@2477 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/src/lib_fltk-1.1.10/CMake/PlatformTests.cxx
+++ ivp/src/lib_fltk-1.1.10/CMake/PlatformTests.cxx
@@ -79,3 +79,15 @@ int main()
 	return 0;
 }
 #endif
+
+#ifdef HAVE_SCANDIR_POSIX
+#include <dirent.h>
+int func (const char *d, dirent ***list, void *sort) {
+    int n = scandir(d, list, 0, (int(*)(const dirent **, const dirent **))sort);
+}
+
+int main()
+{
+   return 0;
+}
+#endif
