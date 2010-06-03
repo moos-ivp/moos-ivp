da0b9633e8dbf13b0bc584cc8ce329319df44e04
Chris Gagner <christopher.gagner@navy.mil>
2010-06-03 19:14:05 +0000

    Updated the pBasicContactMgr to build under windows.
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@2733 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/src/pBasicContactMgr/BasicContactMgr.cpp
+++ ivp/src/pBasicContactMgr/BasicContactMgr.cpp
@@ -5,6 +5,10 @@
 /*    DATE: Feb 24th 2010                                   */
 /************************************************************/
 
+#ifdef _WIN32
+  #define _USE_MATH_DEFINES
+#endif
+
 #include <cmath>
 #include <iterator>
 #include <vector>
