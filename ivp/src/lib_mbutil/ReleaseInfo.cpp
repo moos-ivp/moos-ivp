03b6ab34fb08d6c1baa8460ca6fc42ffe4c7df17
Mike Benjamin <mikerb@csail.mit.edu>
2011-07-18 11:10:03 +0000

    updated release info after 4.2
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@3523 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/src/lib_mbutil/ReleaseInfo.cpp
+++ ivp/src/lib_mbutil/ReleaseInfo.cpp
@@ -34,7 +34,7 @@ void showReleaseInfo(string app, string license_info)
 {
   string pad = padString("", (16-app.length()));
   cout << "************************************************************************" << endl;
-  cout << "* " + app + " - MOOS-IvP Release Bundle - VERSION 4.1+HEAD" + pad + "  r3498 *" << endl;
+  cout << "* " + app + " - MOOS-IvP Release Bundle - VERSION 4.2+HEAD" + pad + "  r3523 *" << endl;
   cout << "* M.Benjamin, H.Schmidt and J.Leonard (MIT), P.Newman (Oxford)         *" << endl;
   if(license_info == "gpl") {
     cout << "* Copyright (C) 2008 Free Software Foundation, Inc.                    *" << endl;
