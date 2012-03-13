79c6579eb11c9c2a342e3d578819d7427e4b8404
Mike Benjamin <mikerb@csail.mit.edu>
2012-03-13 21:56:35 +0000

    lawnmower east-west mod in trunk
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@4276 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/src/lib_geometry/XYFormatUtilsSegl.cpp
+++ ivp/src/lib_geometry/XYFormatUtilsSegl.cpp
@@ -383,6 +383,11 @@ XYSegList stringLawnmower2SegList(string str)
   if(!rads_set && !degs_set)
     return(null_seglist);
 
+  if(rows == "east-west")
+    rows = "ew";
+  else if(rows == "north-south")
+    rows = "ns";
+
   if((rows != "ew") && (rows != "ns"))
     rows = "ew";
 
