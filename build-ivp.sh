47138845d33e3db26a93e9766f79800e3d29a238
Mike Benjamin <mikerb@csail.mit.edu>
2013-10-18 18:30:59 +0000

    modified the build-ivp.sh script to make it easier to invoke debug/release/none options. Default is no longer debug, but rather none
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@6045 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- build-ivp.sh
+++ build-ivp.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
 INVOCATION_ABS_DIR="`pwd`"
+CONFIG="None"
+CLEAN="no"
 
-./configure-ivp.sh Debug
+for ARGI; do
+    if [ "${ARGI}" = "--help" -o "${ARGI}" = "-h" ] ; then
+        printf "%s [SWITCHES]  \n" $0
+        printf "  --help, -h            \n"
+        printf "  --debug,   -d           \n"
+        printf "  --release, -r           \n"
+        printf "  clean                 \n"
+        exit 0;
+    elif [ "${ARGI}" = "--debug" -o "${ARGI}" = "-d" ] ; then
+        CONFIG="Debug"
+    elif [ "${ARGI}" = "--release" -o "${ARGI}" = "-r" ] ; then
+        CONFIG="Release"
+    elif [ "${ARGI}" = "clean" ] ; then
+        CLEAN="yes"
+    else
+        printf "Bad Argument: %s \n" $ARGI
+        exit 0
+    fi
+done
+
+./configure-ivp.sh ${CONFIG}
 
 cd build
 
@@ -10,6 +32,11 @@ echo ""
 echo "Invoking make..."
 echo ""
 
-make -j12 $@
+if [ "${CLEAN}" = "yes" ] ; then
+    make clean
+else
+    make -j12 
+
+fi
 
 cd ${INVOCATION_ABS_DIR}
