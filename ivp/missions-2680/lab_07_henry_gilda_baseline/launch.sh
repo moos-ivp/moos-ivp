c6d86e3061e584e91f3bb07e6b6aac0c596a913f
Mike Benjamin <mikerb@csail.mit.edu>
2014-03-01 18:25:02 +0000

    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@6179 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/missions-2680/lab_07_henry_gilda_baseline/launch.sh
+++ ivp/missions-2680/lab_07_henry_gilda_baseline/launch.sh
@@ -32,23 +32,21 @@ LOITER_POS2="x=125,y=-50"
 
 SHORE_LISTEN="9300"
 
-if [ "${SHOREONLY}" != "yes" ]; then
-    nsplug meta_vehicle.moos targ_$VAME1.moos -f WARP=$TIME_WARP \
-	VNAME=$VNAME1      START_POS=$START_POS1                \
-	VPORT="9001"       SHARE_LISTEN="9301"                  \
-	VTYPE="kayak"      SHORE_LISTEN=$SHORE_LISTEN           
+nsplug meta_vehicle.moos targ_$VNAME1.moos -f WARP=$TIME_WARP \
+    VNAME=$VNAME1      START_POS=$START_POS1                  \
+    VPORT="9001"       SHARE_LISTEN="9301"                    \
+    VTYPE="kayak"      SHORE_LISTEN=$SHORE_LISTEN           
 
-    nsplug meta_vehicle.moos targ_$VNAME2.moos -f WARP=$TIME_WARP \
-	VNAME=$VNAME2      START_POS=$START_POS2                \
-	VPORT="9002"       SHARE_LISTEN="9302"                  \
-	VTYPE="kayak"      SHORE_LISTEN=$SHORE_LISTEN           
+nsplug meta_vehicle.moos targ_$VNAME2.moos -f WARP=$TIME_WARP \
+    VNAME=$VNAME2      START_POS=$START_POS2                  \
+    VPORT="9002"       SHARE_LISTEN="9302"                    \
+    VTYPE="kayak"      SHORE_LISTEN=$SHORE_LISTEN            
 
-    nsplug meta_vehicle.bhv targ_$VNAME1.bhv -f VNAME=$VNAME1     \
-	START_POS=$START_POS1 LOITER_POS=$LOITER_POS1       
+nsplug meta_vehicle.bhv targ_$VNAME1.bhv -f VNAME=$VNAME1     \
+    START_POS=$START_POS1 LOITER_POS=$LOITER_POS1       
 
-    nsplug meta_vehicle.bhv targ_$VNAME2.bhv -f VNAME=$VNAME2     \
-	START_POS=$START_POS2 LOITER_POS=$LOITER_POS2       
-fi    
+nsplug meta_vehicle.bhv targ_$VNAME2.bhv -f VNAME=$VNAME2     \
+    START_POS=$START_POS2 LOITER_POS=$LOITER_POS2       
 
 nsplug meta_shoreside.moos targ_shoreside.moos -f WARP=$TIME_WARP \
    VNAME="shoreside"  SHARE_LISTEN=$SHORE_LISTEN  VPORT="9000"     
