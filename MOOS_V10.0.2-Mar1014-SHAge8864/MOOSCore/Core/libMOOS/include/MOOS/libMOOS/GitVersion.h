420360ecdd19ea60feb0b35526752fda56c9013f
Mike Benjamin <mikerb@csail.mit.edu>
2014-03-10 17:53:18 +0000

    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@6206 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- MOOS_V10.0.2-Mar1014-SHAge8864/MOOSCore/Core/libMOOS/include/MOOS/libMOOS/GitVersion.h
+++ MOOS_V10.0.2-Mar1014-SHAge8864/MOOSCore/Core/libMOOS/include/MOOS/libMOOS/GitVersion.h
@@ -1,4 +1,4 @@
 #pragma once
 //this file is auto generated and defines a 
 //string capturing the current git SHA1 code
-#define MOOS_GIT_VERSION "libMOOS git SHA1 is 10.0.2-devel-4-ge8864"
+#define MOOS_GIT_VERSION "libMOOS  git SHA1 is 10.0.2-devel-4-ge8864"
