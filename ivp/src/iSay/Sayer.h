98a4bbe8cc77c3e0c7e520bdf74e60a73bb126fa
Mike Benjamin <mikerb@csail.mit.edu>
2014-07-23 02:04:16 +0000

    updated iSay license info
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/releases/moos-ivp-14.7@6490 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/src/iSay/Sayer.h
+++ ivp/src/iSay/Sayer.h
@@ -1,9 +1,25 @@
-/************************************************************/
-/*  NAME: Mike Benjamin                                     */
-/*  ORGN: Dept of Mechanical Eng / CSAIL, MIT Cambridge MA  */
-/*  FILE: Sayer.h                                           */
-/*  DATE: May 20th, 2013                                    */
-/************************************************************/
+/*****************************************************************/
+/*    NAME: Michael Benjamin, Henrik Schmidt, and John Leonard   */
+/*    ORGN: Dept of Mechanical Eng / CSAIL, MIT Cambridge MA     */
+/*    FILE: Sayer.h                                              */
+/*    DATE: May 20th, 2013                                       */
+/*                                                               */
+/* This file is part of MOOS-IvP                                 */
+/*                                                               */
+/* MOOS-IvP is free software: you can redistribute it and/or     */
+/* modify it under the terms of the GNU General Public License   */
+/* as published by the Free Software Foundation, either version  */
+/* 3 of the License, or (at your option) any later version.      */
+/*                                                               */
+/* MOOS-IvP is distributed in the hope that it will be useful,   */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty   */
+/* of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See  */
+/* the GNU General Public License for more details.              */
+/*                                                               */
+/* You should have received a copy of the GNU General Public     */
+/* License along with MOOS-IvP.  If not, see                     */
+/* <http://www.gnu.org/licenses/>.                               */
+/*****************************************************************/
 
 #ifndef IVOICE_HEADER
 #define IVOICE_HEADER
@@ -55,3 +71,4 @@ class Sayer : public AppCastingMOOSApp
 };
 
 #endif 
+
--- ivp/src/iSay/Utterance.h
+++ ivp/src/iSay/Utterance.h
@@ -1,9 +1,25 @@
-/************************************************************/
-/*  NAME: Mike Benjamin                                     */
-/*  ORGN: Dept of Mechanical Eng / CSAIL, MIT Cambridge MA  */
-/*  FILE: Utterance.h                                       */
-/*  DATE: May 20th, 2013                                    */
-/************************************************************/
+/*****************************************************************/
+/*    NAME: Michael Benjamin, Henrik Schmidt, and John Leonard   */
+/*    ORGN: Dept of Mechanical Eng / CSAIL, MIT Cambridge MA     */
+/*    FILE: Utterance.h                                          */
+/*    DATE: May 20th, 2013                                       */
+/*                                                               */
+/* This file is part of MOOS-IvP                                 */
+/*                                                               */
+/* MOOS-IvP is free software: you can redistribute it and/or     */
+/* modify it under the terms of the GNU General Public License   */
+/* as published by the Free Software Foundation, either version  */
+/* 3 of the License, or (at your option) any later version.      */
+/*                                                               */
+/* MOOS-IvP is distributed in the hope that it will be useful,   */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty   */
+/* of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See  */
+/* the GNU General Public License for more details.              */
+/*                                                               */
+/* You should have received a copy of the GNU General Public     */
+/* License along with MOOS-IvP.  If not, see                     */
+/* <http://www.gnu.org/licenses/>.                               */
+/*****************************************************************/
 
 #ifndef UTTERANCE_HEADER
 #define UTTERANCE_HEADER
@@ -65,3 +81,4 @@ bool operator< (const Utterance& utter1, const Utterance& utter2);
 bool operator> (const Utterance& utter1, const Utterance& utter2);
 
 #endif 
+
