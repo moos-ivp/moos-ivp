0ae8a26a061322917771d362becdbdab6406ed75
Mike Benjamin <mikerb@csail.mit.edu>
2014-08-01 17:29:32 +0000

    updated release number in 14.7.1 ReleaseInfo.cpp
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/releases/moos-ivp-14.7.1@6556 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/src/lib_mbutil/ReleaseInfo.cpp
+++ ivp/src/lib_mbutil/ReleaseInfo.cpp
@@ -38,7 +38,7 @@ void showReleaseInfo(string app, string license_info)
     pad = padString("", (22-app.length()));
   cout << "********************************************************************" << endl;
   cout << "* " + app + " - Part of the MOOS-IvP Release Bundle     " +  pad +"*" << endl;
-  cout << "* Version 14.7.x, Released July 22nd, 2014, www.moos-ivp.org          *" << endl;
+  cout << "* Version 14.7.1, Released July 30th, 2014, www.moos-ivp.org          *" << endl;
   cout << "* M.Benjamin, H.Schmidt and J.Leonard (MIT), P.Newman (Oxford)     *" << endl;
   if(license_info == "gpl") {
     cout << "* This is free software; see the source for copying conditions.    *" << endl;
