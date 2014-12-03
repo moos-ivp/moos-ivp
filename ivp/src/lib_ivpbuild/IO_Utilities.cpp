e7c81e0765572b22deedebeae3303ab798ddcede
Mike Benjamin <mikerb@csail.mit.edu>
2014-12-03 13:06:45 +0000

    minor fix to IO_Utilities.cpp to address harmless run warning
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@6742 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/src/lib_ivpbuild/IO_Utilities.cpp
+++ ivp/src/lib_ivpbuild/IO_Utilities.cpp
@@ -135,6 +135,8 @@ vector<IvPFunction*> readFunctions(const string& str)
     // Pretend we care about the fscanf result to avoid compiler warning
     int result=0; 
     result = fscanf(f, "%c", &c);
+    if(result == 0)
+      cout << "matching failure" << endl;
     if(c == 'F') {
       int buff_ix = 0;
       while(c != '\n') {
