c5e16a7ec2aacaf062a3b5e061ea6612bacc3f79
Mike Benjamin <mikerb@csail.mit.edu>
2015-02-01 17:21:22 +0000

    update to uProcessWatch main.cpp
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@6787 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/src/uProcessWatch/main.cpp
+++ ivp/src/uProcessWatch/main.cpp
@@ -60,7 +60,7 @@ int main(int argc, char *argv[])
   cout << termColor() << endl;
 
   ProcessWatch process_watch;
-  process_watch.Run(run_command.c_str(), mission_file.c_str(), argc, argv);
+  process_watch.Run(run_command.c_str(), mission_file.c_str());
   
   return(0);
 }
