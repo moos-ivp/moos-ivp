de842b153cdc62b1f58d9e2725705c95765a3c19
Mike Benjamin <mikerb@csail.mit.edu>
2015-12-28 13:09:03 +0000

    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@7428 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/missions/m2_berta_detect/pare.sh
+++ ivp/missions/m2_berta_detect/pare.sh
@@ -4,53 +4,45 @@ PARE_WINDOW=15
 MARK_LIST="ENCOUNTER"
 PARE_LIST="BHV_IPF,VIEW_SEGLIST"
 HIT_LIST="*ITER_GAP,*ITER_LEN,PSHARE*,NODE_REPORT*,DB_QOS"
+RMORIG="no"
+ALL_OK="yes"
 
 for ARGI; do
     if [ "${ARGI}" = "--help" -o "${ARGI}" = "-h" ] ; then
-	printf "%s [SWITCHES] [time_warp]   \n" $0
-	printf "  --just_make, -j    \n" 
-	printf "  --help, -h         \n" 
+	printf "%s [SWITCHES] [PARE_WINDOW]                   \n" $0
+	printf "  --help, -h                                  \n" 
+	printf "  --rmo            Remove Original Alog Files \n" 
 	exit 0;
     elif [ "${ARGI//[^0-9]/}" = "$ARGI" -a "$TIME_WARP" = 1 ]; then 
         PARE_WINDOW=$ARGI
-    elif [ "${ARGI}" = "v1" ] ; then
-	VARIATION="1"
+    elif [ "${ARGI}" = "--rmo" ] ; then
+	RMORIG="yes"
     else 
 	printf "Bad Argument: %s \n" $ARGI
 	exit 0
     fi
 done
 
-echo "=========================================================ABE"
-cd LOG_A*; 
-alogscan --rate_only LOG*.alog
-alogpare LOG*.alog abe_pared.alog --pare_window=$PARE_WINDOW \
-    --markvars=$MARK_LIST --hitvars=$HIT_LIST --parevars=$PARE_LIST
-alogscan --rate_only abe_pared.alog
-cd ..
 
-echo "=========================================================BEN"
-cd LOG_B*; 
-alogscan --rate_only LOG*.alog
-alogpare LOG*.alog ben_pared.alog --pare_window=$PARE_WINDOW \
-    --markvars=$MARK_LIST --hitvars=$HIT_LIST --parevars=$PARE_LIST
-alogscan --rate_only ben_pared.alog
-cd ..
 
-echo "=========================================================CAL"
-cd LOG_C*; 
-alogscan --rate_only LOG*.alog
-alogpare LOG*.alog cal_pared.alog --pare_window=$PARE_WINDOW \
-    --markvars=$MARK_LIST --hitvars=$HIT_LIST --parevars=$PARE_LIST
-alogscan --rate_only cal_pared.alog
-cd ..
+for dirs in LOG*/ ; do 
+    cd $dirs
+    alogscan --rate_only LOG*.alog
+    alogpare LOG*.alog name.alog --pare_window=$PARE_WINDOW \
+	--markvars=$MARK_LIST --hitvars=$HIT_LIST --parevars=$PARE_LIST
+    rc=$?
+    if [[ $rc != 0 ]]; then
+	echo "pare failed: $rc"; ALL_OK="no"
+    fi
+    alogscan --rate_only *_pared.alog
+    cd ..
+    echo "=================================================="
+done
 
-echo "=========================================================DEB"
-cd LOG_D*; 
-alogscan --rate_only LOG*.alog
-alogpare LOG*.alog deb_pared.alog --pare_window=$PARE_WINDOW \
-    --markvars=$MARK_LIST --hitvars=$HIT_LIST --parevars=$PARE_LIST
-alogscan --rate_only deb_pared.alog
-cd ..
+if [ "$ALL_OK" = "yes" -a "$RMORIG" = "yes" ]; then 
+    echo Removing original alog files...
+    #rm -f */LOG*.alog
+fi
 
-echo "============================================ DONE ========="
+exit 0
+
