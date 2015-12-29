cc78b9dc29f3e6d00ae66ccf05d5e590e17d12f9
Mike Benjamin <mikerb@csail.mit.edu>
2015-12-29 20:57:14 +0000

    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@7434 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

---
+++ ivp/missions/m2_berta_detect/xlaunch.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -e
+
+
+#-------------------------------------------------------
+#  Part 1: Check for and handle command-line arguments
+#-------------------------------------------------------
+TIME_WARP=1
+JUST_MAKE="no"
+for ARGI; do
+    if [ "${ARGI}" = "--help" -o "${ARGI}" = "-h" ] ; then
+	printf "%s [SWITCHES] [time_warp]   \n" $0
+	printf "  --help, -h         \n" 
+	exit 0;
+    elif [ "${ARGI//[^0-9]/}" = "$ARGI" -a "$TIME_WARP" = 1 ]; then 
+        TIME_WARP=$ARGI
+    else 
+	printf "Bad Argument: %s \n" $ARGI
+	exit 0
+    fi
+done
+
+./launch.sh $TIME_WARP >& /dev/null &
+sleep 5
+uPokeDB targ_shoreside.moos DEPLOY_ALL=true MOOS_MANUAL_OVERRIDE_ALL=false
+sleep 5
+
+COND="\"((DB_UPTIME > 500) or (ENCOUNTER_TOTAL > 4))\""
+
+while true; do 
+    if uQueryDB targ_shoreside.moos --condition="ENCOUNTER_TOTAL > 40" ; then 
+	echo "Quitting...."
+	ktm >& /dev/null
+	exit 0
+    fi
+    sleep 5
+    echo "continuing...."
+done
