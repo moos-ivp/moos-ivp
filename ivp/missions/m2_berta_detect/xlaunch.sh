058373175eab3b7285cea6ac2491ec0c828d70c5
Mike Benjamin <mikerb@csail.mit.edu>
2016-01-18 21:33:53 +0000

    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@7495 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/missions/m2_berta_detect/xlaunch.sh
+++ ivp/missions/m2_berta_detect/xlaunch.sh
@@ -5,31 +5,47 @@
 #  Part 1: Check for and handle command-line arguments
 #-------------------------------------------------------
 TIME_WARP=1
-JUST_MAKE="no"
+INHIBIT_GUI="no"
+AMT=1000
 for ARGI; do
     if [ "${ARGI}" = "--help" -o "${ARGI}" = "-h" ] ; then
-	printf "%s [SWITCHES] [time_warp]   \n" $0
-	printf "  --help, -h         \n" 
+	printf "%s [SWITCHES] [time_warp]             \n" $0
+	printf "  --help, -h                          \n" 
+	printf "  --no_gui, -n   (no pMarienViewer)   \n" 
+	printf "  --amt=N        (total encounters)   \n" 
 	exit 0;
     elif [ "${ARGI//[^0-9]/}" = "$ARGI" -a "$TIME_WARP" = 1 ]; then 
         TIME_WARP=$ARGI
+    elif [ "${ARGI}" = "--no_gui" -o "${ARGI}" = "-n" ] ; then
+	INHIBIT_GUI="yes"
+    elif [ "${ARGI:0:6}" = "--amt=" ] ; then
+        AMT="${ARGI#--amt=*}"
     else 
 	printf "Bad Argument: %s \n" $ARGI
 	exit 0
     fi
 done
 
-./launch.sh $TIME_WARP >& /dev/null &
+if [ "${INHIBIT_GUI}" = "yes" ] ; then
+    ./launch.sh --no_gui $TIME_WARP >& /dev/null &
+else
+    ./launch.sh $TIME_WARP >& /dev/null &
+fi
+    
 sleep 5
 uPokeDB targ_shoreside.moos DEPLOY_ALL=true MOOS_MANUAL_OVERRIDE_ALL=false
 sleep 5
 
-COND="\"((DB_UPTIME > 500) or (ENCOUNTER_TOTAL > 4))\""
+# Run for at most 5 hours or at most $AMT encounters
+#COND="\"((DB_UPTIME > 18000) or (ENCOUNTER_TOTAL > $AMT))\""
 
 while true; do 
-    if uQueryDB targ_shoreside.moos --condition="ENCOUNTER_TOTAL > 1000" ; then 
+    if uQueryDB targ_shoreside.moos --condition="ENCOUNTER_TOTAL > $AMT" ; then 
 	echo "Quitting...."
 	ktm >& /dev/null
+	echo "Beginning pare...."
+	./pare.sh --rmo
+	echo "Pare complete."
 	exit 0
     fi
     sleep 30
