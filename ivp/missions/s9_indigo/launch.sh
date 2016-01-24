6a345106889a95ab6f3b5f18526996c5cc9f3914
Mike Benjamin <mikerb@csail.mit.edu>
2016-01-24 00:00:40 +0000

    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@7553 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/missions/s9_indigo/launch.sh
+++ ivp/missions/s9_indigo/launch.sh
@@ -1,31 +1,69 @@
 #!/bin/bash -e
 
-TIME_WARP=1
+WARP=1
+HELP="no"
+BAD_ARGS=""
+
+#-------------------------------------------------------
+#  Part 1: Check for and handle command-line arguments
+#-------------------------------------------------------
+let COUNT=0
 for ARGI; do
+    UNDEFINED_ARG=$ARGI
+    if [ "${ARGI:0:6}" = "--warp" ] ; then
+	WARP="${ARGI#--warp=*}"
+	UNDEFINED_ARG=""
+    fi
     if [ "${ARGI}" = "--help" -o "${ARGI}" = "-h" ] ; then
-	printf "%s [SWITCHES]                                       \n" $0
-	printf "Switches:                                               \n" 
-	printf "  --warp=WARP_VALUE                                     \n" 
-	printf "  --help, -h                                            \n" 
-	printf "Notes:                                                  \n"
-	printf " (1) If one numerical argument given, it is interpreted \n"
-	printf "     as being a warp value request.                     \n"
-	printf "     ./launch.sh 12  same as:  ./launch.sh --warp=12    \n"
-	exit 0;
-    elif [ "${ARGI//[^0-9]/}" = "$ARGI" -a "$TIME_WARP" = 1 ]; then 
-        TIME_WARP=$ARGI
-    else 
-	printf "Bad Argument: %s \n" $ARGI
-	exit 0
+	HELP="yes"
+	UNDEFINED_ARG=""
+    fi
+    # Handle Warp shortcut
+    if [ "${ARGI//[^0-9]/}" = "$ARGI" -a "$COUNT" = 0 ]; then 
+        WARP=$ARGI
+        let "COUNT=$COUNT+1"
+        UNDEFINED_ARG=""
+    fi
+    if [ "${UNDEFINED_ARG}" != "" ] ; then
+	BAD_ARGS=$UNDEFINED_ARG
     fi
 done
+
+if [ "${BAD_ARGS}" != "" ] ; then
+    printf "Bad Argument: %s \n" $BAD_ARGS
+    exit 0
+fi
+
+if [ "${HELP}" = "yes" ]; then
+    printf "%s [SWITCHES]                                        \n" $0
+    printf "Switches:                                               \n" 
+    printf "  --warp=WARP_VALUE                                     \n" 
+    printf "  --help, -h                                            \n" 
+    printf "Notes:                                                  \n"
+    printf " (1) If one numerical argument given, it is interpreted \n"
+    printf "     as being a warp value request.                     \n"
+    printf "     ./launch.sh 12  same as:  ./launch.sh --warp=12    \n"
+    exit 0;
+fi
+
+# Second check that the warp argument is numerical
+if [ "${WARP//[^0-9]/}" != "$WARP" ]; then 
+    printf "Warp values must be numerical. Exiting now."
+    exit 127
+fi
+
+#-------------------------------------------------------
+#  Part 2: Create the .moos and .bhv files. 
+#-------------------------------------------------------
+
+# Not applicable in this example
 
 #-------------------------------------------------------
 #  Part 3: Launch the processes
 #-------------------------------------------------------
 
-printf "Launching ... (WARP=%s) \n" $TIME_WARP
-pAntler indigo.moos --MOOSTimeWarp=$TIME_WARP >& /dev/null &
+printf "Launching ... (WARP=%s) \n" $WARP
+pAntler indigo.moos --MOOSTimeWarp=$WARP >& /dev/null &
 printf "Done Launching... \n"
 
 
