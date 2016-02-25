341c3129757340a2717ab38a7a6cbb20c9cca6f5
Mike Benjamin <mikerb@csail.mit.edu>
2016-02-25 01:11:13 +0000

    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@7585 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/src/app_cpabench/main.cpp
+++ ivp/src/app_cpabench/main.cpp
@@ -22,49 +22,92 @@ void showHelpAndExit();
 
 int main(int argc, char *argv[])
 { 
+  bool old_cpa  = false;
+  bool new_cpa  = true;
+  bool both_cpa = false;
+
   for(int i=1; i<argc; i++) {
     string argi = argv[i];
     if((argi == "-h") || (argi == "--help"))
       showHelpAndExit();
+    if((argi == "-o") || (argi == "--old")) {
+      old_cpa = true;
+      new_cpa = false;
+    }
+    if((argi == "-b") || (argi == "--both")) {
+      both_cpa = true;
+      new_cpa = false;
+    }
   }
 
   double cnx = 100;
   double cny = 100;
-  double cnh = 45;
-  double cnv = 1.0;
   double osx = 200;
   double osy = 104;
 
   MBTimer timer;
   timer.start();
 
-  CPAEngine engine(cny, cnx, cnh, cnv, osy, osx);
-
-  double cpa = engine.evalCPA(315, 1.0, 60);
-  
-  cout << "Example sanity check cpa: " << cpa << endl;
-
   unsigned int total = 0;
-  for(int k=0; k<50; k++) {
-    for(double i=0; i<5; i=i+0.001) {
-      for(double j=0; j<360; j=j+1) {
-	total++;
-	double cpa = engine.evalCPA(j, i, 60);
-	if(cpa == 0)
-	  cpa++;
+
+  if(new_cpa) {
+    for(double cnv=0; cnv<2.5; cnv+=0.1) {
+      for(double cnh=0; cnh<360; cnh+=1) {      
+	CPAEngineX enginex(cny, cnx, cnh, cnv, osy, osx);
+	for(double osv=0; osv<2.5; osv+=0.1) {
+	  for(double osh=0; osh<360; osh=osh+1) {
+	    total++;
+	    enginex.evalCPA(osh, osv, 60);
+	  }
+	}
+      }
+    }
+  }
+
+  if(old_cpa) {
+    for(double cnv=0; cnv<2.5; cnv+=0.1) {
+      for(double cnh=0; cnh<360; cnh+=1) {      
+	CPAEngine engine(cny, cnx, cnh, cnv, osy, osx);
+	for(double osv=0; osv<2.5; osv+=0.1) {
+	  for(double osh=0; osh<360; osh=osh+1) {
+	    total++;
+	    engine.evalCPA(osh, osv, 60);
+	  }
+	}
+      }
+    }
+  }
+  
+  if(both_cpa) {
+    for(double cnv=0; cnv<2.5; cnv+=0.1) {
+      for(double cnh=0; cnh<360; cnh+=1) {      
+	CPAEngineX enginex(cny, cnx, cnh, cnv, osy, osx);
+	CPAEngine  engine(cny, cnx, cnh, cnv, osy, osx);
+	for(double osv=0; osv<2.5; osv+=0.1) {
+	  for(double osh=0; osh<360; osh=osh+1) {
+	    total++;
+	    double cpax = enginex.evalCPA(osh, osv, 60);
+	    double cpa  = engine.evalCPA(osh, osv, 60);
+	    double delta = cpa - cpax;
+	    if(delta < 0)
+	      delta = -delta;
+	    if(delta > 0.00001)
+	      cout << endl << "Mismatch: cpa:" << cpa << " cpax:" << cpax << endl;
+	  }
+	}
       }
     }
   }
 
   timer.stop();
-  double elapsed_time = timer.get_float_wall_time();
+  double elapsed_time = timer.get_float_cpu_time();
 
   double rate = ((double)(total)) / elapsed_time;
 
   unsigned long int ul_rate = rate;
 
   cout << endl << "done, total=" << uintToCommaString(total) << endl;
-  cout << "rate: " << ulintToCommaString(ul_rate) << " cpa calculations per sec." << endl;
+  cout << "rate: " << ulintToCommaString(ul_rate) << " cpa calcs per sec." << endl;
 
   return(0);
 }
@@ -75,8 +118,10 @@ int main(int argc, char *argv[])
 
 void showHelpAndExit()
 { 
-  cout << "Usage:                                         " << endl;
-  cout << "  cpabench                                     " << endl;
+  cout << "Usage:                                                   " << endl;
+  cout << "  cpabench            (Runs the test  CPAEngineX)        " << endl;
+  cout << "  cpabench --old,-o   (Runs the trunk CPAEngine)         " << endl;
+  cout << "  cpabench --both,-b  (Runs both, checks for mismatches) " << endl;
 
   exit(0);
 }
