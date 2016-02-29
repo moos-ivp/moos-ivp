9bb5907f948ab351e5bddb9fa5df2d3df5030849
Mike Benjamin <mikerb@csail.mit.edu>
2016-02-29 19:49:52 +0000

    mod src/CMakeLists.txt
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@7605 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/src/app_cpabench/main.cpp
+++ ivp/src/app_cpabench/main.cpp
@@ -7,6 +7,7 @@
 #include "MBUtils.h"
 #include "MBTimer.h"
 #include "AngleUtils.h"
+#include "GeomUtils.h"
 #include <cmath>
 #include <stdlib.h>
 #include <iostream>
@@ -27,6 +28,8 @@ int main(int argc, char *argv[])
   bool new_cpa  = true;
   bool both_cpa = false;
 
+  int  loops = 1;
+
   for(int i=1; i<argc; i++) {
     string argi = argv[i];
     if((argi == "-h") || (argi == "--help"))
@@ -35,31 +38,40 @@ int main(int argc, char *argv[])
       old_cpa = true;
       new_cpa = false;
     }
-    if((argi == "-b") || (argi == "--both")) {
+    else if((argi == "-b") || (argi == "--both")) {
       both_cpa = true;
       new_cpa = false;
+    }
+    else if(isNumber(argi)) {
+      loops = atoi(argi.c_str());
     }
   }
 
   double cnx = 100;
   double cny = 100;
   double osx = 200;
-  double osy = 104;
+  double osy = 150;
 
   MBTimer timer;
   timer.start();
 
   unsigned int total = 0;
 
+  //CPAEngineX test_engine(cny, cnx, 45, 2.0, osy, osx);
+  //exit(0);
+
+  
   if(new_cpa) {
-    CPAEngineX enginex;
-    for(double cnv=0; cnv<2.5; cnv+=0.1) {
-      for(double cnh=0; cnh<360; cnh+=1) {      
-	enginex.reset(cny, cnx, cnh, cnv, osy, osx);
-	for(double osv=0; osv<2.5; osv+=0.1) {
-	  for(double osh=0; osh<360; osh=osh+1) {
-	    total++;
-	    enginex.evalCPA(osh, osv, 60);
+    for(int i=0; i<loops; i++) {
+      CPAEngineX enginex;
+      for(double cnv=0; cnv<2.5; cnv+=0.1) {
+	for(double cnh=0; cnh<360; cnh+=1) {      
+	  enginex.reset(cny, cnx, cnh, cnv, osy, osx);
+	  for(double osv=0; osv<2.5; osv+=0.1) {
+	    for(double osh=0; osh<360; osh=osh+1) {
+	      total++;
+	      enginex.evalCPA(osh, osv, 60);
+	    }
 	  }
 	}
       }
@@ -67,13 +79,15 @@ int main(int argc, char *argv[])
   }
 
   if(old_cpa) {
-    for(double cnv=0; cnv<2.5; cnv+=0.1) {
-      for(double cnh=0; cnh<360; cnh+=1) {      
-	CPAEngineOld engine(cny, cnx, cnh, cnv, osy, osx);
-	for(double osv=0; osv<2.5; osv+=0.1) {
-	  for(double osh=0; osh<360; osh=osh+1) {
-	    total++;
-	    engine.evalCPA(osh, osv, 60);
+    for(int i=0; i<loops; i++) {
+      for(double cnv=0; cnv<2.5; cnv+=0.1) {
+	for(double cnh=0; cnh<360; cnh+=1) {      
+	  CPAEngineOld engine(cny, cnx, cnh, cnv, osy, osx);
+	  for(double osv=0; osv<2.5; osv+=0.1) {
+	    for(double osh=0; osh<360; osh=osh+1) {
+	      total++;
+	      engine.evalCPA(osh, osv, 60);
+	    }
 	  }
 	}
       }
@@ -88,14 +102,31 @@ int main(int argc, char *argv[])
 	CPAEngine  engine(cny, cnx, cnh, cnv, osy, osx);
 	for(double osv=0; osv<2.5; osv+=0.1) {
 	  for(double osh=0; osh<360; osh=osh+1) {
-	    total++;
-	    double cpax = enginex.evalCPA(osh, osv, 60);
-	    double cpa  = engine.evalCPA(osh, osv, 60);
+	    total+=2;;
+	    double roc, rocx;
+	    double cpax = enginex.evalCPA(osh, osv, 60, &rocx);
+	    double cpa  = engine.evalCPA(osh, osv, 60, &roc);
 	    double delta = cpa - cpax;
 	    if(delta < 0)
 	      delta = -delta;
-	    if(delta > 0.00001)
+	    double delta_roc = roc - rocx;
+	    if(delta_roc < 0)
+	      delta_roc = -delta_roc;
+	    if(delta_roc > 0.00001) {
+	      cout << endl << "Mismatch: roc:" << roc << " rocx:" << rocx << endl;
+	      cout << "CPAEngine::evalROC(): " << engine.evalROC(osh, osv) << endl;
+	      cout << "CPAEngineX::evalROC(): " << enginex.evalROC(osh, osv) << endl;
+	    }
+	    if(delta > 0.00001) {
 	      cout << endl << "Mismatch: cpa:" << cpa << " cpax:" << cpax << endl;
+	      cout << "Range: " << distPointToPoint(osx, osy, cnx, cny) << endl;
+	      cout << "Contact: " << endl;
+	      cout << "  cnv: " << cnv << endl;
+	      cout << "  cnh: " << cnh << endl;
+	      cout << "Ownship: " << endl;
+	      cout << "  osv: " << osv << endl;
+	      cout << "  osh: " << osh << endl << endl;
+	    }
 	  }
 	}
       }
