585ee0fbe1d90c046cd5416440ff82d1639fe11f
Mike Benjamin <mikerb@csail.mit.edu>
2016-10-08 23:22:57 +0000

    removed debug code from GeomUtils.cpp
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aro/trunk@7767 7b6de600-d9ab-45c9-a1ca-e8145d3c6daf

--- ivp/src/lib_geometry/GeomUtils.cpp
+++ ivp/src/lib_geometry/GeomUtils.cpp
@@ -32,11 +32,6 @@ using namespace std;
 //---------------------------------------------------------------
 // Procedure: distPointToPoint
 
-double distPointToPointXXX(double x1, double y1, double x2, double y2)
-{
-  return(hypot((x1-x2), (y1-y2)));
-}
-  
 double distPointToPoint(double x1, double y1, double x2, double y2)
 {
   return(hypot((x1-x2), (y1-y2)));
